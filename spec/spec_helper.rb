7e0d53410e8e237fe3fb4b31022c3ba6a624c7d5
Bogdan Irimie <irimie.bogdan@puppet.com>
2019-08-06 17:52:40 +0300

    (FACT-1953) Set code coverage lower limit.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,8 @@ SimpleCov.start do
   track_files 'lib/**/*.rb'
 end
 
+SimpleCov.minimum_coverage 90
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
