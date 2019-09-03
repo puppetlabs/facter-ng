e490ead0eac83707edb3a95ad1f416266793a12e
Sebastian Miclea <sebastian.miclea@puppet.com>
2019-09-03 16:48:44 +0300

    (FACT-2010) Added Os fact for Fedora

--- lib/resolvers/selinux_resolver.rb
+++ lib/resolvers/selinux_resolver.rb
@@ -12,6 +12,7 @@ class SELinuxResolver < BaseResolver
     def resolve(fact_name)
       @@semaphore.synchronize do
         result ||= @@fact_list[fact_name]
+<<<<<<< HEAD
         result || read_lsb_release_file(fact_name)
       end
     end
@@ -29,6 +30,24 @@ class SELinuxResolver < BaseResolver
       end
 
       @@fact_list[fact_name]
+=======
+
+        return result unless result.nil?
+
+        output, _s = Open3.capture2('cat /proc/self/mounts')
+        @@fact_list[:enabled] = false
+
+        output.each_line do |line|
+          next unless line.match?(/selinuxfs/)
+
+          @@fact_list[:enabled] = true
+          @@fact_list[:mountpoint] = line
+          break
+        end
+
+        @@fact_list[fact_name]
+      end
+>>>>>>> (FACT-2010) Added Os fact for Fedora
     end
 
     def invalidate_cache
