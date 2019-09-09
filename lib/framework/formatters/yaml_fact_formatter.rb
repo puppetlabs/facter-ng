dff08d8b62442c312134094dbe210be6aef3f6d5
Bogdan Irimie <bogdan.irimie@puppet.com>
2019-09-09 21:25:06 +0300

    (FACT-2003) Make yaml formatter compatible with the C++ facter version.

--- lib/framework/formatters/yaml_fact_formatter.rb
+++ lib/framework/formatters/yaml_fact_formatter.rb
@@ -2,8 +2,15 @@
 
 module Facter
   class YamlFactFormatter
+    def initialize
+      @log = Log.new
+    end
+
     def format(fact_hash)
-      YAML.dump(JSON.parse(JsonFactFormatter.new.format(fact_hash)))
+      yaml_pretty = YAML.dump(JSON.parse(JsonFactFormatter.new.format(fact_hash)))
+
+      @log.debug('Replace ---  from yaml beginning, to keep it compatible with C facter')
+      yaml_pretty.gsub(/^---[\r\n]+/, '')
     end
   end
 end
