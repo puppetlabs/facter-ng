321a07eed61c4c864f8b320fa4253f3c5517c4fe
Bogdan Irimie <bogdan.irimie@puppet.com>
2019-09-27 09:49:21 +0300

    (FACT-2043) Migrate mocks from mocha to rspec mocks in execution.

--- spec/custom_facts/core/execution_spec.rb
+++ spec/custom_facts/core/execution_spec.rb
@@ -5,37 +5,37 @@ describe Facter::Core::Execution do
   let(:impl) { described_class.impl }
 
   it "delegates #search_paths to the implementation" do
-    impl.expects(:search_paths)
+    expect(impl).to receive(:search_paths)
     subject.search_paths
   end
 
   it "delegates #which to the implementation" do
-    impl.expects(:which).with('waffles')
+    expect(impl).to receive(:which).with('waffles')
     subject.which('waffles')
   end
 
   it "delegates #absolute_path? to the implementation" do
-    impl.expects(:absolute_path?).with('waffles', nil)
+    expect(impl).to receive(:absolute_path?).with('waffles', nil)
     subject.absolute_path?('waffles')
   end
 
   it "delegates #absolute_path? with an optional platform to the implementation" do
-    impl.expects(:absolute_path?).with('waffles', :windows)
+    expect(impl).to receive(:absolute_path?).with('waffles', :windows)
     subject.absolute_path?('waffles', :windows)
   end
 
   it "delegates #expand_command to the implementation" do
-    impl.expects(:expand_command).with('waffles')
+    expect(impl).to receive(:expand_command).with('waffles')
     subject.expand_command('waffles')
   end
 
   it "delegates #exec to #execute" do
-    impl.expects(:execute).with('waffles', {:on_fail => nil})
+    expect(impl).to receive(:execute).with('waffles', {:on_fail => nil})
     subject.exec('waffles')
   end
 
   it "delegates #execute to the implementation" do
-    impl.expects(:execute).with('waffles', {})
+    expect(impl).to receive(:execute).with('waffles', {})
     subject.execute('waffles')
   end
 end
