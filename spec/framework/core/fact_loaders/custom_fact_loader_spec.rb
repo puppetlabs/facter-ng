023ebf1e2fd73f00de9d97390b58c70aa636b491
Bogdan Irimie <bogdan.irimie@puppet.com>
2019-10-08 10:49:13 +0300

    (FACT-2046) Add custom fact loader spec.

---
+++ spec/framework/core/fact_loaders/custom_fact_loader_spec.rb
@@ -0,0 +1,24 @@
+
+describe 'CustomFactLoader' do
+  describe '#initialize' do
+    let(:collection) { double(LegacyFacter::Util::Collection) }
+
+    before do
+      allow(LegacyFacter).to receive(:collection).and_return(collection)
+    end
+
+    it 'loads one custom fact' do
+      allow(collection).to receive(:custom_facts).and_return({'os' => nil})
+      custom_fact_loader = Facter::CustomFactLoader.new
+
+      expect(custom_fact_loader.custom_facts).to eq({'os' => nil})
+    end
+
+    it 'loads no custom facts' do
+      allow(collection).to receive(:custom_facts).and_return({})
+      custom_fact_loader = Facter::CustomFactLoader.new
+
+      expect(custom_fact_loader.custom_facts).to eq({})
+    end
+  end
+end
