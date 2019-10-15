4b3a0a914ac938afee78ba43acb9080c9be6845d
gigicampean <56248530+gigicampean@users.noreply.github.com>
2019-10-15 14:41:56 +0300

    (FACT-2071) Gave timezone example.

--- spec/facter/facts/fedora/timezone_spec.rb
+++ spec/facter/facts/fedora/timezone_spec.rb
@@ -3,9 +3,10 @@
 describe 'Fedora Timezone' do
   context '#call_the_resolver' do
     it 'returns a fact' do
-      expected_fact = double(Facter::ResolvedFact, name: 'timezone', value: 'value')
-      allow(Facter::Resolvers::Timezone).to receive(:resolve).with(:timezone).and_return('value')
-      allow(Facter::ResolvedFact).to receive(:new).with('timezone', 'value').and_return(expected_fact)
+      value = 'EEST'
+      expected_fact = double(Facter::ResolvedFact, name: 'timezone', value: value)
+      allow(Facter::Resolvers::Timezone).to receive(:resolve).with(:timezone).and_return(value)
+      allow(Facter::ResolvedFact).to receive(:new).with('timezone', value).and_return(expected_fact)
 
       fact = Facter::Fedora::Timezone.new
       expect(fact.call_the_resolver).to eq(expected_fact)
