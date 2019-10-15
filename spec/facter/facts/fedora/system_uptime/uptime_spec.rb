80aae1b086f5a746f5c5bc54f6ccd316573890aa
gigicampean <56248530+gigicampean@users.noreply.github.com>
2019-10-15 15:12:37 +0300

    Update uptime_spec.rb

--- spec/facter/facts/fedora/system_uptime/uptime_spec.rb
+++ spec/facter/facts/fedora/system_uptime/uptime_spec.rb
@@ -3,7 +3,7 @@
 describe 'Fedora SystemUptimeUptime' do
   context '#call_the_resolver' do
     it 'returns a fact' do
-      value ='0'
+      value ='4:27 hours'
 
       expected_fact = double(Facter::ResolvedFact, name: 'system_uptime.uptime', value: value)
       allow(Facter::Resolvers::Uptime).to receive(:resolve).with(:uptime).and_return(value)
