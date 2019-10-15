5178501db7bf0b67438b602bda116843e0bb09f0
gigicampean <56248530+gigicampean@users.noreply.github.com>
2019-10-15 14:25:06 +0300

    (FACT-2069) Changed PATH example.

--- spec/facter/facts/fedora/path_spec.rb
+++ spec/facter/facts/fedora/path_spec.rb
@@ -3,9 +3,12 @@
 describe 'Fedora Path' do
   context '#call_the_resolver' do
     it 'returns a fact' do
-      expected_fact = double(Facter::ResolvedFact, name: 'path', value: '/Users/User/.rvm/gems/ruby-2.4.6/bin:/Users/User/.rvm/gems/ruby-2.4.6@global/bin:/Users/User/.rvm/rubies/ruby-2.4.6/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/puppetlabs/bin:/Users/User/.rvm/bin')
-      allow(Facter::Resolvers::Path).to receive(:resolve).with(:path).and_return('value')
-      allow(Facter::ResolvedFact).to receive(:new).with('path', 'value').and_return(expected_fact)
+      
+      value = '/Users/User/.rvm/gems/ruby-2.4.6/bin:/Users/User/.rvm/gems/ruby-2.4.6@global/bin'
+      
+      expected_fact = double(Facter::ResolvedFact, name: 'path', value: value)
+      allow(Facter::Resolvers::Path).to receive(:resolve).with(:path).and_return(value)
+      allow(Facter::ResolvedFact).to receive(:new).with('path', value).and_return(expected_fact)
 
       fact = Facter::Fedora::Path.new
       expect(fact.call_the_resolver).to eq(expected_fact)
