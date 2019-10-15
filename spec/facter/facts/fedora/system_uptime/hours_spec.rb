82fc91bcd905878ab99d9e53a73b88d4365e8f23
gigicampean <56248530+gigicampean@users.noreply.github.com>
2019-10-15 14:59:41 +0300

    (FACT-2073) Changed examples.

--- spec/facter/facts/fedora/system_uptime/hours_spec.rb
+++ spec/facter/facts/fedora/system_uptime/hours_spec.rb
@@ -5,9 +5,9 @@ describe 'Fedora SystemUptimeHours' do
     it 'returns a fact' do
       value = '0'
 
-      expected_fact = double(Facter::ResolvedFact, name: 'system_uptime.hours', value: 'value')
-      allow(Facter::Resolvers::Uptime).to receive(:resolve).with(:hours).and_return('value')
-      allow(Facter::ResolvedFact).to receive(:new).with('system_uptime.hours', 'value').and_return(expected_fact)
+      expected_fact = double(Facter::ResolvedFact, name: 'system_uptime.hours', value: value)
+      allow(Facter::Resolvers::Uptime).to receive(:resolve).with(:hours).and_return(value)
+      allow(Facter::ResolvedFact).to receive(:new).with('system_uptime.hours', value).and_return(expected_fact)
 
       fact = Facter::Fedora::SystemUptimeHours.new
       expect(fact.call_the_resolver).to eq(expected_fact)
