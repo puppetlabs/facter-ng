ad57bc33c90482fb8148a064e0b2645004abda0d
gigicampean <56248530+gigicampean@users.noreply.github.com>
2019-10-15 14:29:51 +0300

    (FACT-2070) Gave a SITEDIR example.

--- spec/facter/facts/fedora/ruby/sitedir_spec.rb
+++ spec/facter/facts/fedora/ruby/sitedir_spec.rb
@@ -3,9 +3,12 @@
 describe 'Fedora RubySitedir' do
   context '#call_the_resolver' do
     it 'returns a fact' do
-      expected_fact = double(Facter::ResolvedFact, name: 'ruby.sitedir', value: 'value')
-      allow(Facter::Resolvers::Ruby).to receive(:resolve).with(:sitedir).and_return('value')
-      allow(Facter::ResolvedFact).to receive(:new).with('ruby.sitedir', 'value').and_return(expected_fact)
+      
+      value = '/opt/puppetlabs/puppet/lib/ruby/site_ruby/2.5.0'
+      
+      expected_fact = double(Facter::ResolvedFact, name: 'ruby.sitedir', value: value)
+      allow(Facter::Resolvers::Ruby).to receive(:resolve).with(:sitedir).and_return(value)
+      allow(Facter::ResolvedFact).to receive(:new).with('ruby.sitedir', value).and_return(expected_fact)
 
       fact = Facter::Fedora::RubySitedir.new
       expect(fact.call_the_resolver).to eq(expected_fact)
