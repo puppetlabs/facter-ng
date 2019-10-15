b0937cfa96a3ee81e2327af6b79aa4515d4ba338
gigicampean <56248530+gigicampean@users.noreply.github.com>
2019-10-15 14:38:35 +0300

    (FACT-2070) Excess spaces eliminated.

--- spec/facter/facts/fedora/ruby/platform_spec.rb
+++ spec/facter/facts/fedora/ruby/platform_spec.rb
@@ -3,9 +3,8 @@
 describe 'Fedora RubyPlatform' do
   context '#call_the_resolver' do
     it 'returns a fact' do
-      
       value = 'x86_64-darwin18'
-      
+
       expected_fact = double(Facter::ResolvedFact, name: 'ruby.platform', value: value)
       allow(Facter::Resolvers::Ruby).to receive(:resolve).with(:platform).and_return(value)
       allow(Facter::ResolvedFact).to receive(:new).with('ruby.platform', value).and_return(expected_fact)
