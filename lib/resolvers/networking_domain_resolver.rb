74e877bb308455c0126b573e106bbd37072f989c
Jordi <jordi@thejournal.ie>
2019-10-16 14:52:15 +0100

    Add networking.domain resolver for Ubuntu

---
+++ lib/resolvers/networking_domain_resolver.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module Facter
+  module Resolvers
+    class NetworkingDomain < BaseResolver
+      # :networking_domain
+
+      @semaphore = Mutex.new
+      @fact_list ||= {}
+
+      class << self
+        def resolve(fact_name)
+          @semaphore.synchronize do
+            result ||= @fact_list[fact_name]
+            subscribe_to_manager
+            result || read_networking_domain(fact_name)
+          end
+        end
+
+        def read_networking_domain(fact_name)
+          output, _status = Open3.capture2('cat /etc/resolv.conf')
+          output.each_line do |line|
+            next unless line.match(/search\s+(\S+)/)
+
+            @fact_list[:networking_domain] = Regexp.last_match(1)
+            break
+          end
+          @fact_list[fact_name]
+        end
+      end
+    end
+  end
+end
