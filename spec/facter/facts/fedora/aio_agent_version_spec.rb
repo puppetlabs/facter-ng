c07f1300156c769a56e5036c58888bf00b3d8a61
gigicampean <56248530+gigicampean@users.noreply.github.com>
2019-10-16 16:19:23 +0300

    Update aio_agent_version_spec.rb

--- spec/facter/facts/fedora/aio_agent_version_spec.rb
+++ spec/facter/facts/fedora/aio_agent_version_spec.rb
@@ -4,7 +4,7 @@ describe 'Fedora AioAgentVersion' do
   context '#call_the_resolver' do
     it 'returns a fact' do
       value = '6.0.9'
-      
+
       expected_fact = double(Facter::ResolvedFact, name: 'aio_agent_version', value: value)
       allow(Facter::Resolvers::Agent).to receive(:resolve).with(:aio_agent_version).and_return(value)
       allow(Facter::ResolvedFact).to receive(:new).with('aio_agent_version', value).and_return(expected_fact)
