20c291a10495da877b24fd69adb3cd62a7dd8cb7
Campean Gheorghe Liviu <gigi.campean@puppet.com>
2019-10-22 15:14:19 +0300

    (FACT-2080) Playing around rubocop.

--- spec/facter/facts/fedora/memory/swap/available_bytes_spec.rb
+++ spec/facter/facts/fedora/memory/swap/available_bytes_spec.rb
@@ -5,7 +5,9 @@ describe 'Fedora MemorySwapAvailableBytes' do
     it 'returns a fact' do
       expected_fact = double(Facter::ResolvedFact, name: 'memory.swap.available_bytes', value: 'value')
       allow(Facter::Resolvers::Linux::Memory).to receive(:resolve).with(:swap_free).and_return('value')
-      allow(Facter::ResolvedFact).to receive(:new).with('memory.swap.available_bytes', 'value').and_return(expected_fact)
+      allow(Facter::ResolvedFact).to receive(:new).with('memory.swap.available_bytes', 'value').and_return(
+        expected_fact
+      )
 
       fact = Facter::Fedora::MemorySwapAvailableBytes.new
       expect(fact.call_the_resolver).to eq(expected_fact)
