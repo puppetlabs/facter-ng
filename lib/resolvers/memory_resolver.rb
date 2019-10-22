54fbbad7e8989d20b6667fd0cccc93bfb9527e95
Campean Gheorghe Liviu <gigi.campean@puppet.com>
2019-10-22 16:08:19 +0300

    (FACT-2080) Corrected code.

--- lib/resolvers/memory_resolver.rb
+++ lib/resolvers/memory_resolver.rb
@@ -16,7 +16,7 @@ module Facter
             end
           end
 
-          def read_meminfo_file(_fact_name)
+          def read_meminfo_file(fact_name)
             output, _status = Open3.capture2('cat /proc/meminfo')
             output.each_line do |line|
               result = line.match(/\d+/).to_s.to_i * 1024
@@ -25,13 +25,13 @@ module Facter
               @fact_list[:swap_total] = result if line.match(/SwapTotal:/)
               @fact_list[:swap_free] = result if line.match(/SwapFree:/)
             end
+            @fact_list[:used_bytes] = (@fact_list[:total] - @fact_list[:memfree])
+            @fact_list[:sused_bytes] = (@fact_list[:swap_total] - @fact_list[:swap_free])
+            @fact_list[:capacity] = ((@fact_list[:used_bytes] / @fact_list[:total]) * (100 / 100))
+            @fact_list[:scapacity] = ((@fact_list[:sused_bytes] / @fact_list[:swap_total]) * (100 / 100))
+            @fact_list[fact_name]
           end
-          @fact_list[:used_bytes] = (@fact_list[:total] - @fact_list[:memfree]) * 1024
-          @fact_list[:sused_bytes] = (@fact_list[:swap_total] - @fact_list[:swap_free]) * 1024
-          @fact_list[:capacity] = ((@fact_list[:used_bytes] / @fact_list[:total]) * (100 / 100)) * 1024
-          @fact_list[:scapacity] = ((@fact_list[:sused_bytes] / @fact_list[:swap_total]) * (100 / 100)) * 1024
-          @fact_list[fact_name]
-          end
+        end
       end
     end
   end
