7e269c6a32b89a238ed7ab5da3139b33fb7820a5
Jørn Ølmheim <jorn@olmheim.com>
2019-10-29 13:31:47 +0100

    Added braces to join.

--- lib/facts/ubuntu/kernelversion.rb
+++ lib/facts/ubuntu/kernelversion.rb
@@ -7,7 +7,7 @@ module Facter
 
       def call_the_resolver
         version_numbers = Resolvers::Uname.resolve(:kernelrelease).scan(/\d+/)
-        fact_value = version_numbers[0..2].join '.'
+        fact_value = version_numbers[0..2].join('.')
         ResolvedFact.new(FACT_NAME, fact_value)
       end
     end
