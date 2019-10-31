45ed847223fb37e1ea57d64f4f1a4d3df0ac29b8
Bogdan Irimie <bogdan.irimie@puppet.com>
2019-10-31 16:26:23 +0200

    (FACT-2097) Use lowercase when loading directory for facts.

--- lib/framework/core/file_loader.rb
+++ lib/framework/core/file_loader.rb
@@ -33,7 +33,7 @@ load_lib_dirs('framework', 'core', 'fact', 'external')
 os = ENV['RACK_ENV'] == 'test' ? '' : CurrentOs.instance.identifier
 
 os_hierarchy = CurrentOs.instance.hierarchy
-os_hierarchy.each { |operating_system| load_lib_dirs('facts', operating_system, '**') }
+os_hierarchy.each { |operating_system| load_lib_dirs('facts', operating_system.downcase, '**') }
 
 load_lib_dirs('resolvers', os.to_s, '**') if os.to_s =~ /win|aix|solaris/
 
