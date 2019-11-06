2735c055c7369c864fe212499a35488faef6297f
Campean Gheorghe Liviu <gigi.campean@puppet.com>
2019-11-06 15:08:09 +0200

    (FACT-2099) New test version.

--- lib/resolvers/dmi_resolver.rb
+++ lib/resolvers/dmi_resolver.rb
@@ -3,7 +3,7 @@
 module Facter
   module Resolvers
     module Linux
-      class DMIBios < BaseResolver
+      class DmiBios < BaseResolver
         @log = Facter::Log.new
         @semaphore = Mutex.new
         @fact_list ||= {}
@@ -32,26 +32,22 @@ module Facter
 
           private
 
-          def read_facts(_fact_name)
-            files = %w[bios_date bios_vendor bios_version board_vendor board_name board_serial chassis_asset_tag
-                       chassis_type sys_vendor product_name product_serial product_uuid]
+          def read_facts(fact_name)
+            files = %w[bios_date bios_vendor bios_version board_vendor board_name board_serial
+                       chassis_asset_tag chassis_type sys_vendor product_name product_serial
+                       product_uuid]
             types = ['Other', 'Unknown', 'Desktop', 'Low Profile Desktop', 'Pizza Box', 'Mini Tower', 'Tower',
                      'Portable', 'Laptop', 'Notebook', 'Hand Held', 'Docking Station', 'All in One', 'Sub Notebook',
                      'Space-Saving', 'Lunch Box', 'Main System Chassis', 'Expansion Chassis', 'SubChassis',
                      'Bus Expansion Chassis', 'Peripheral Chassis', 'Storage Chassis', 'Rack Mount Chassis',
                      'Sealed-Case PC', 'Multi-system', 'CompactPCI', 'AdvancedTCA', 'Blade', 'Blade Enclosure',
                      'Tablet', 'Convertible', 'Detachable']
+            return unless File.directory?('/sys/class/dmi')
+
             files.each do |element|
-              unless File.exist?("/sys/class/dmi/id/#{element}")
-                @log.debug "File /sys/class/dmi/id/#{element} does not exist!"
-                next
-              end
-              if element == 'chassis_type'
-                token = File.read("/sys/class/dmi/id/chassis_type")
-                @fact_list[:chassis_type] = types[token - 1]
-              end
               @fact_list[element.to_sym] = File.read("/sys/class/dmi/id/#{element}")
             end
+            @fact_list[:chassis_type] = types[@fact_list[:chassis_type].to_i]
             @fact_list[fact_name]
           end
         end
--- spec/facter/resolvers/dmi_resolver_spec.rb
+++ spec/facter/resolvers/dmi_resolver_spec.rb
@@ -1,112 +1,196 @@
 # frozen_string_literal: true
 
 describe 'DmiResolver' do
-  let(:bios_release_date) { '12/12/2018' }
-  let(:bios_vendor) { 'Phoenix Technologies LTD' }
-  let(:bios_version) { '6.00' }
-  let(:board_manufacturer) { 'Intel Corporation' }
-  let(:board_product) { '440BX Desktop Reference Platform' }
-  let(:board_serial_number) { 'None' }
-  let(:chassis_asset_tag) { 'No Asset Tag' }
-  let(:chassis_type) { 'Other' }
-  let(:manufacturer) { 'VMware, Inc.' }
-  let(:product_name) { 'VMware Virtual Platform' }
-  let(:product_serial_number) { 'VMware-42 1a 02 ea e6 27 76 b8-a1 23 a7 8a d3 12 ee cf' }
-  let(:product_uuid) { 'ea021a42-27e6-b876-a123-a78ad312eecf' }
+  describe '#resolve' do
+    let(:files) do
+      %w[bios_date bios_vendor bios_version board_vendor board_name board_serial
+         chassis_asset_tag chassis_type sys_vendor product_name product_serial
+         product_uuid]
+    end
+    let(:test_dir) { '/sys/class/dmi' }
+    before do
+      allow(File).to receive(:directory?).with(test_dir).and_return(true)
+    end
 
-  it 'returns bios_release_date' do
-    allow(File).to receive(:exist?).with('/sys/class/dmi/id/bios_date').and_return(true)
-    allow(File).to receive(:read).with('/sys/class/dmi/id/bios_date').and_return(bios_release_date)
-    result = Facter::Resolvers::Linux::DMIBios.resolve(:bios_date)
+    context 'when directory does not exist' do
+      it 'when directory does not exist' do
+        allow(File).to receive(:directory?).with(test_dir).and_return(false)
+      end
+    end
 
-    expect(result).to eq(bios_release_date)
-  end
+    context 'when bios_date file exists' do
+      let(:test_file) { test_dir + 'id/bios_date' }
+      let(:bios_release_date) { '12/12/2018' }
 
-  it 'returns bios_vendor' do
-    allow(File).to receive(:exist?).with('/sys/class/dmi/id/bios_vendor').and_return(true)
-    allow(File).to receive(:read).with('/sys/class/dmi/id/bios_vendor').and_return(bios_vendor)
-    result = Facter::Resolvers::Linux::DMIBios.resolve(:bios_vendor)
+      it 'returns bios_release_date' do
+        allow(File).to receive(:read).with('/sys/class/dmi/id/bios_date').and_return(bios_release_date)
+        files.each do |file|
+          allow(File).to receive(:read).with("/sys/class/dmi/id/#{file}") unless test_file
+                                                                                 .match?(file)
+        end
+        result = Facter::Resolvers::Linux::DmiBios.resolve(:bios_date)
 
-    expect(result).to eq(bios_vendor)
-  end
+        expect(result).to eq(bios_release_date)
+      end
+    end
 
-  it 'returns bios_version' do
-    allow(File).to receive(:exist?).with('/sys/class/dmi/id/bios_version').and_return(true)
-    allow(File).to receive(:read).with('/sys/class/dmi/id/bios_version').and_return(bios_version)
-    result = Facter::Resolvers::Linux::DMIBios.resolve(:bios_version)
+    context 'when bios_vendor file exists' do
+      let(:test_file) { test_dir + 'id/bios_vendor' }
+      let(:bios_vendor) { 'Phoenix Technologies LTD' }
 
-    expect(result).to eq(bios_version)
-  end
+      it 'returns bios_vendor' do
+        allow(File).to receive(:read).with('/sys/class/dmi/id/bios_vendor').and_return(bios_vendor)
+        files.each do |file|
+          allow(File).to receive(:read).with("/sys/class/dmi/id/#{file}") unless test_file
+                                                                                 .match?(file)
+        end
+        result = Facter::Resolvers::Linux::DmiBios.resolve(:bios_vendor)
 
-  it 'returns board_manufacturer' do
-    allow(File).to receive(:exist?).with('/sys/class/dmi/id/board_vendor').and_return(true)
-    allow(File).to receive(:read).with('/sys/class/dmi/id/board_vendor').and_return(board_manufacturer)
-    result = Facter::Resolvers::Linux::DMIBios.resolve(:board_vendor)
+        expect(result).to eq(bios_vendor)
+      end
+    end
+    context 'when bios_version file exists' do
+      let(:test_file) { test_dir + 'id/bios_version' }
+      let(:bios_version) { '6.00' }
 
-    expect(result).to eq(board_manufacturer)
-  end
+      it 'returns bios_version' do
+        allow(File).to receive(:read).with('/sys/class/dmi/id/bios_version').and_return(bios_version)
+        files.each do |file|
+          allow(File).to receive(:read).with("/sys/class/dmi/id/#{file}") unless test_file
+                                                                                 .match?(file)
+        end
+        result = Facter::Resolvers::Linux::DmiBios.resolve(:bios_version)
 
-  it 'returns board_product' do
-    allow(File).to receive(:exist?).with('/sys/class/dmi/id/board_name').and_return(true)
-    allow(File).to receive(:read).with('/sys/class/dmi/id/board_name').and_return(board_product)
-    result = Facter::Resolvers::Linux::DMIBios.resolve(:board_name)
+        expect(result).to eq(bios_version)
+      end
+    end
+    context 'when board_vendor file exists' do
+      let(:test_file) { test_dir + 'id/board_vendor' }
+      let(:board_manufacturer) { 'Intel Corporation' }
 
-    expect(result).to eq(board_product)
-  end
+      it 'returns board_manufacturer' do
+        allow(File).to receive(:read).with('/sys/class/dmi/id/board_vendor').and_return(board_manufacturer)
+        files.each do |file|
+          allow(File).to receive(:read).with("/sys/class/dmi/id/#{file}") unless test_file
+                                                                                 .match?(file)
+        end
+        result = Facter::Resolvers::Linux::DmiBios.resolve(:board_vendor)
 
-  it 'returns board_serial_number' do
-    allow(File).to receive(:exist?).with('/sys/class/dmi/id/board_serial').and_return(true)
-    allow(File).to receive(:read).with('/sys/class/dmi/id/board_serial').and_return(board_serial_number)
-    result = Facter::Resolvers::Linux::DMIBios.resolve(:board_serial)
+        expect(result).to eq(board_manufacturer)
+      end
+    end
+    context 'when board_name file exists' do
+      let(:test_file) { test_dir + 'id/board_name' }
+      let(:board_name) { '440BX Desktop Reference Platform' }
 
-    expect(result).to eq(board_serial_number)
-  end
+      it 'returns board_product' do
+        allow(File).to receive(:read).with('/sys/class/dmi/id/board_name').and_return(board_name)
+        files.each do |file|
+          allow(File).to receive(:read).with("/sys/class/dmi/id/#{file}") unless test_file
+                                                                                 .match?(file)
+        end
+        result = Facter::Resolvers::Linux::DmiBios.resolve(:board_name)
 
-  it 'returns chassis_asset_tag' do
-    allow(File).to receive(:exist?).with('/sys/class/dmi/id/chassis_asset_tag').and_return(true)
-    allow(File).to receive(:read).with('/sys/class/dmi/id/chassis_asset_tag').and_return(chassis_asset_tag)
-    result = Facter::Resolvers::Linux::DMIBios.resolve(:chassis_asset_tag)
+        expect(result).to eq(board_name)
+      end
+    end
+    context 'when board_serial file exists' do
+      let(:test_file) { test_dir + 'id/board_serial' }
+      let(:board_serial_number) { 'None' }
 
-    expect(result).to eq(chassis_asset_tag)
-  end
+      it 'returns board_serial_number' do
+        allow(File).to receive(:read).with('/sys/class/dmi/id/board_serial').and_return(board_serial_number)
+        files.each do |file|
+          allow(File).to receive(:read).with("/sys/class/dmi/id/#{file}") unless test_file.match?(file)
+        end
+        result = Facter::Resolvers::Linux::DmiBios.resolve(:board_serial)
 
-  it 'returns chassis_type' do
-    allow(File).to receive(:exist?).with('/sys/class/dmi/id/chassis_type').and_return(true)
-    allow(File).to receive(:read).with('/sys/class/dmi/id/chassis_type').and_return(chassis_type)
-    result = Facter::Resolvers::Linux::DMIBios.resolve(:chassis_type)
+        expect(result).to eq(board_serial_number)
+      end
+    end
+    context 'when chassis_asset_tag file exists' do
+      let(:test_file) { test_dir + 'id/chassis_asset_tag' }
+      let(:chassis_asset_tag) { 'No Asset Tag' }
 
-    expect(result).to eq(chassis_type)
-  end
+      it 'returns chassis_asset_tag' do
+        allow(File).to receive(:read).with('/sys/class/dmi/id/chassis_asset_tag').and_return(chassis_asset_tag)
+        files.each do |file|
+          allow(File).to receive(:read).with("/sys/class/dmi/id/#{file}") unless test_file.match?(file)
+        end
+        result = Facter::Resolvers::Linux::DmiBios.resolve(:chassis_asset_tag)
 
-  it ' returns manufacturer' do
-    allow(File).to receive(:exist?).with('/sys/class/dmi/id/sys_vendor').and_return(true)
-    allow(File).to receive(:read).with('/sys/class/dmi/id/sys_vendor').and_return(manufacturer)
-    result = Facter::Resolvers::Linux::DMIBios.resolve(:sys_vendor)
+        expect(result).to eq(chassis_asset_tag)
+      end
+    end
+    context 'when chassis_type file exists' do
+      let(:test_file) { test_dir + 'id/chassis_type' }
+      let(:chassis_type) { 'Other' }
 
-    expect(result).to eq(manufacturer)
-  end
+      it 'returns chassis_type' do
+        allow(File).to receive(:read).with('/sys/class/dmi/id/chassis_type').and_return(chassis_type)
+        files.each do |file|
+          allow(File).to receive(:read).with("/sys/class/dmi/id/#{file}") unless test_file.match?(file)
+        end
+        result = Facter::Resolvers::Linux::DmiBios.resolve(:chassis_type)
 
-  it 'returns product_name' do
-    allow(File).to receive(:exist?).with('/sys/class/dmi/id/product_name').and_return(true)
-    allow(File).to receive(:read).with('/sys/class/dmi/id/product_name').and_return(product_name)
-    result = Facter::Resolvers::Linux::DMIBios.resolve(:product_name)
+        expect(result).to eq(chassis_type)
+      end
+    end
+    context 'when sys_vendor file exists' do
+      let(:test_file) { test_dir + 'id/sys_vendor' }
+      let(:manufacturer) { 'VMware, Inc.' }
 
-    expect(result).to eq(product_name)
-  end
+      it ' returns manufacturer' do
+        allow(File).to receive(:read).with('/sys/class/dmi/id/sys_vendor').and_return(manufacturer)
+        files.each do |file|
+          allow(File).to receive(:read).with("/sys/class/dmi/id/#{file}") unless test_file.match?(file)
+        end
+        result = Facter::Resolvers::Linux::DmiBios.resolve(:sys_vendor)
 
-  it 'returns product_serial_number' do
-    allow(File).to receive(:exist?).with('/sys/class/dmi/id/product_serial').and_return(true)
-    allow(File).to receive(:read).with('/sys/class/dmi/id/product_serial').and_return(product_serial_number)
-    result = Facter::Resolvers::Linux::DMIBios.resolve(:product_serial)
+        expect(result).to eq(manufacturer)
+      end
+    end
+    context 'when product_name file exists' do
+      let(:test_file) { test_dir + 'id/product_name' }
+      let(:product_name) { 'VMware Virtual Platform' }
 
-    expect(result).to eq(product_serial_number)
-  end
+      it 'returns product_name' do
+        allow(File).to receive(:read).with('/sys/class/dmi/id/product_name').and_return(product_name)
+        files.each do |file|
+          allow(File).to receive(:read).with("/sys/class/dmi/id/#{file}") unless test_file.match?(file)
+        end
+        result = Facter::Resolvers::Linux::DmiBios.resolve(:product_name)
 
-  it 'returns product_uuid' do
-    allow(File).to receive(:exist?).with('/sys/class/dmi/id/product_uuid').and_return(true)
-    allow(File).to receive(:read).with('/sys/class/dmi/id/product_uuid').and_return(product_uuid)
-    result = Facter::Resolvers::Linux::DMIBios.resolve(:product_uuid)
+        expect(result).to eq(product_name)
+      end
+    end
+    context 'when product_serial file exists' do
+      let(:test_file) { test_dir + 'id/product_serial' }
+      let(:product_serial_number) { 'VMware-42 1a 02 ea e6 27 76 b8-a1 23 a7 8a d3 12 ee cf' }
 
-    expect(result).to eq(product_uuid)
+      it 'returns product_serial_number' do
+        allow(File).to receive(:read).with('/sys/class/dmi/id/product_serial').and_return(product_serial_number)
+        files.each do |file|
+          allow(File).to receive(:read).with("/sys/class/dmi/id/#{file}") unless test_file.match?(file)
+        end
+        result = Facter::Resolvers::Linux::DmiBios.resolve(:product_serial)
+
+        expect(result).to eq(product_serial_number)
+      end
+    end
+    context 'when product_uuid file exists' do
+      let(:test_file) { test_dir + 'id/product_uuid' }
+      let(:product_uuid) { 'ea021a42-27e6-b876-a123-a78ad312eecf' }
+
+      it 'returns product_uuid' do
+        allow(File).to receive(:read).with('/sys/class/dmi/id/product_uuid').and_return(product_uuid)
+        files.each do |file|
+          allow(File).to receive(:read).with("/sys/class/dmi/id/#{file}") unless test_file.match?(file)
+        end
+        result = Facter::Resolvers::Linux::DmiBios.resolve(:product_uuid)
+
+        expect(result).to eq(product_uuid)
+      end
+    end
   end
 end
