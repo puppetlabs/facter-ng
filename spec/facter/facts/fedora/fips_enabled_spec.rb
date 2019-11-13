5ed1a5bfdad50a86b7448f35ff511997507423c7
Campean Gheorghe Liviu <gigi.campean@puppet.com>
2019-11-13 18:14:41 +0200

    (FACT-2121) Corrected resolver placement in code.

--- spec/facter/facts/fedora/fips_enabled_spec.rb
+++ spec/facter/facts/fedora/fips_enabled_spec.rb
@@ -6,7 +6,7 @@ describe 'Fedora FipsEnabled' do
       value = 'false'
 
       expected_fact = double(Facter::ResolvedFact, name: 'fips_enabled', value: value)
-      allow(Facter::Resolvers::Linux::FipsEnabled).to receive(:fips_enabled).with(<resolver_fact>).and_return(value)
+      allow(Facter::Resolvers::Linux::FipsEnabled).to receive(:resolve).with(:fips_enabled).and_return(value)
       allow(Facter::ResolvedFact).to receive(:new).with('fips_enabled', value).and_return(expected_fact)
 
       fact = Facter::Fedora::FipsEnabled.new
