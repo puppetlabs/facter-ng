d44ccb55c11c6a3e418d99cd8b676104e1b27e0b
Campean Gheorghe Liviu <gigi.campean@puppet.com>
2019-11-13 17:15:59 +0200

    (FACT-2107) Correct saved value of the file.

--- spec/facter/resolvers/fips_enabled_resolver_spec.rb
+++ spec/facter/resolvers/fips_enabled_resolver_spec.rb
@@ -1,27 +1,14 @@
 # frozen_string_literal: true
 
 describe 'FipsEnabledResolver' do
-
-  before do
-    allow(File).to receive(:read)
-      .with('/proc/sys/crypto/fips_enabled')
-      .and_return(fips_enabled)
-  end
-  context 'when fips is enabled' do
-    let(:test_enabled) { '1 '}
-
-    it 'returns fips enabled'
-    result = Facter::Resolvers::Linux::FipsEnabled.resolve(:fips_enabled)
-
-    expect(result).to eq(test_enabled)
-  end
   context 'when fips is not enabled' do
-    let(:fips_enabled) { '0' }
+    let(:fips_enabled) { 'false' }
 
-    it 'returns fips not enabled' do
-    result = Facter::Resolvers::Linux::FipsEnabled.resolve(:fips_enabled)
+    it 'returns fips is not enabled' do
+      allow(File).to receive(:read).with('/proc/sys/crypto/fips_enabled').and_return(fips_enabled)
+      result = Facter::Resolvers::Linux::FipsEnabled.resolve(:fips_enabled)
 
-    expect(result).to eq(fips_enabled)
+      expect(result).to eq(fips_enabled)
     end
   end
 end
