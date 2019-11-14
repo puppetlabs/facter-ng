ffe7bcb8dcfdec41b0a235f56a43db54a3b44ff1
Campean Gheorghe Liviu <gigi.campean@puppet.com>
2019-11-14 16:39:11 +0200

    (FACT-2116) Attempt at displaying facts.

--- spec/facter/facts/fedora/dmi/product/name_spec.rb
+++ spec/facter/facts/fedora/dmi/product/name_spec.rb
@@ -3,9 +3,10 @@
 describe 'Fedora DmiProductName' do
   context '#call_the_resolver' do
     it 'returns a fact' do
-      expected_fact = double(Facter::ResolvedFact, name: 'dmi.product.name', value: 'value')
-      allow(Facter::Resolvers::Linux::DmiBios).to receive(:resolve).with(:product_name).and_return('value')
-      allow(Facter::ResolvedFact).to receive(:new).with('dmi.product.name', 'value').and_return(expected_fact)
+      value = 'VMware Virtual Platform'
+      expected_fact = double(Facter::ResolvedFact, name: 'dmi.product.name', value: value)
+      allow(Facter::Resolvers::Linux::DmiBios).to receive(:resolve).with(:product_name).and_return(value)
+      allow(Facter::ResolvedFact).to receive(:new).with('dmi.product.name', value).and_return(expected_fact)
 
       fact = Facter::Fedora::DmiProductName.new
       expect(fact.call_the_resolver).to eq(expected_fact)
