d951845ea61efcdc1208ff65224916aeef3ca8dc
gigicampean <56248530+gigicampean@users.noreply.github.com>
2019-11-14 11:57:10 +0200

    (FACT-2107) Rubocop fix.

--- spec/facter/resolvers/fips_enabled_resolver_spec.rb
+++ spec/facter/resolvers/fips_enabled_resolver_spec.rb
@@ -3,7 +3,6 @@
 describe 'FipsEnabledResolver' do
   describe '#resolve' do
     context 'when fips is not enabled' do
-
       it 'returns fips is not enabled' do
         allow(File).to receive(:read).with('/proc/sys/crypto/fips_enabled').and_return('0')
         result = Facter::Resolvers::Linux::FipsEnabled.resolve(:fips_enabled)
@@ -13,7 +12,6 @@ describe 'FipsEnabledResolver' do
     end
 
     context 'when fips is enabled' do
-
       it 'returns fips is not enabled' do
         allow(File).to receive(:read).with('/proc/sys/crypto/fips_enabled').and_return('1')
         result = Facter::Resolvers::Linux::FipsEnabled.resolve(:fips_enabled)
