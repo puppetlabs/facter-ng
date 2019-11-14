6133df6d2d63e428cde995f26528b233c9b52e2a
Oana Tanasoiu <oana.tanasoiu@puppet.com>
2019-11-14 10:08:48 +0200

    (FACT-2120) Add kvm fact for Windows

--- lib/facts/windows/hypervisors/kvm.rb
+++ lib/facts/windows/hypervisors/kvm.rb
@@ -1,4 +1,3 @@
-
 # frozen_string_literal: true
 
 module Facter
@@ -7,7 +6,7 @@ module Facter
       FACT_NAME = 'hypervisors.kvm'
 
       def call_the_resolver
-        fact_name = if kvm?
+        fact_value = google_or_openstack || {} if kvm?
 
         ResolvedFact.new(FACT_NAME, fact_value)
       end
@@ -17,34 +16,15 @@ module Facter
       def kvm?
         product_name = Resolvers::DMIComputerSystem.resolve(:name)
 
-        Resolvers::CpuidSource.resolve(:vendor) == 'KVMKVMKVM' &&
-            product_name != 'VirtualBox' && !product_name.match?(/^Parallels/)
+        Resolvers::Virtualization.resolve(:virtual) == 'kvm' &&
+          product_name != 'VirtualBox' && !product_name.match(/^Parallels/)
+      end
+
+      def google_or_openstack
+        return { google: true } if Resolvers::DMIBios.resolve(:manufacturer) == 'Google'
+
+        { openstack: true } if Resolvers::DMIComputerSystem.resolve(:name).match(/^OpenStack/)
       end
     end
   end
 end
-
-
-#  // VirtualBox and Parallels will also report KVM in CPUID, though, so make sure they're not here.
-#         if (cpuid_source.vendor() == "KVMKVMKVM"
-#             && smbios_source.product_name() != "VirtualBox"
-#             && !re_search(smbios_source.product_name(), parallels_pattern)) {
-#             res.validate();
-#
-#             if (smbios_source.bios_vendor() == "Google") {
-#                 res.set("google", true);
-#             }
-#
-#             if (re_search(smbios_source.product_name(), openstack_pattern)) {
-#                 res.set("openstack", true);
-#             }
-#         }
-#
-#
-#
-#   bios_vendor = bios.manufacturer
-#   product_name = computersystemproduct.name
-#
-#
-# static const boost::regex openstack_pattern {"^OpenStack"};
-# static const boost::regex parallels_pattern {"^Parallels"};
