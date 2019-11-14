54ee81da1527af8b46dfac06898b9325db2a55a2
Bogdan Irimie <bogdan.irimie@puppet.com>
2019-11-14 10:12:43 +0200

    (FACT-2107) Implement PR comments.

--- spec/facter/resolvers/fips_enabled_resolver_spec.rb
+++ spec/facter/resolvers/fips_enabled_resolver_spec.rb
@@ -1,14 +1,27 @@
 # frozen_string_literal: true
 
 describe 'FipsEnabledResolver' do
-  context 'when fips is not enabled' do
-    let(:fips_enabled) { 'false' }
+  describe '#resolve' do
+    context 'when fips is not enabled' do
+      let(:fips_enabled) { false }
 
-    it 'returns fips is not enabled' do
-      allow(File).to receive(:read).with('/proc/sys/crypto/fips_enabled').and_return(fips_enabled)
-      result = Facter::Resolvers::Linux::FipsEnabled.resolve(:fips_enabled)
+      it 'returns fips is not enabled' do
+        allow(File).to receive(:read).with('/proc/sys/crypto/fips_enabled').and_return('0')
+        result = Facter::Resolvers::Linux::FipsEnabled.resolve(:fips_enabled)
 
-      expect(result).to eq(fips_enabled)
+        expect(result).to eq(fips_enabled)
+      end
+    end
+
+    context 'when fips is enabled' do
+      let(:fips_enabled) { true }
+
+      it 'returns fips is not enabled' do
+        allow(File).to receive(:read).with('/proc/sys/crypto/fips_enabled').and_return('1')
+        result = Facter::Resolvers::Linux::FipsEnabled.resolve(:fips_enabled)
+
+        expect(result).to eq(fips_enabled)
+      end
     end
   end
 end
