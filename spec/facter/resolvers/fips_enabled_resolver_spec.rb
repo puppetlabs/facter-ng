105275025ad001ecf76827357a4b8f9bb4a97b35
gigicampean <56248530+gigicampean@users.noreply.github.com>
2019-11-14 11:51:47 +0200

    (FACT-2107) Eliminated a variable.

--- spec/facter/resolvers/fips_enabled_resolver_spec.rb
+++ spec/facter/resolvers/fips_enabled_resolver_spec.rb
@@ -3,24 +3,22 @@
 describe 'FipsEnabledResolver' do
   describe '#resolve' do
     context 'when fips is not enabled' do
-      let(:fips_enabled) { false }
 
       it 'returns fips is not enabled' do
         allow(File).to receive(:read).with('/proc/sys/crypto/fips_enabled').and_return('0')
         result = Facter::Resolvers::Linux::FipsEnabled.resolve(:fips_enabled)
 
-        expect(result).to eq(fips_enabled)
+        expect(result).to eq(false)
       end
     end
 
     context 'when fips is enabled' do
-      let(:fips_enabled) { true }
 
       it 'returns fips is not enabled' do
         allow(File).to receive(:read).with('/proc/sys/crypto/fips_enabled').and_return('1')
         result = Facter::Resolvers::Linux::FipsEnabled.resolve(:fips_enabled)
 
-        expect(result).to eq(fips_enabled)
+        expect(result).to eq(true)
       end
     end
   end
