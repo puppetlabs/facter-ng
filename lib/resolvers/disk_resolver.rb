5afdac6467e23f17de0ff8141c3404fbee168729
Campean Gheorghe Liviu <gigi.campean@puppet.com>
2019-11-20 13:46:51 +0200

    (FACT-2129) Rewritten hash and path composing method. Improved coverage a tiny bit.

--- lib/resolvers/disk_resolver.rb
+++ lib/resolvers/disk_resolver.rb
@@ -8,8 +8,7 @@ module Facter
         @semaphore = Mutex.new
         @fact_list ||= {}
         DIR = '/sys/block/'
-        FILE_PATHS = { sr0_model: 'sr0/device/model', sr0_size: 'sr0/size', sr0_vendor: 'sr0/device/vendor',
-                       sda_model: 'sda/device/model', sda_size: 'sda/size', sda_vendor: 'sda/device/vendor' }.freeze
+        FILE_PATHS = { model: '/device/model', size: '/size', vendor: '/device/vendor' }.freeze
         class << self
           # :sr0_model
           # :sr0_size
@@ -27,10 +26,16 @@ module Facter
           end
 
           def read_facts(fact_name)
-            return nil unless File.exist?(DIR + FILE_PATHS[fact_name])
+            file_path = compose_file_path(fact_name)
+            return nil unless File.exist?(file_path)
 
-            result = File.read(DIR + FILE_PATHS[fact_name]).strip
+            result = File.read(file_path).strip
             @fact_list[fact_name] = fact_name.to_s =~ /size/ ? result.to_i * 1024 : result
+          end
+
+          def compose_file_path(fact_name)
+            block, file = fact_name.to_s.split('_')
+            DIR + block + FILE_PATHS[file.to_sym]
           end
         end
       end
