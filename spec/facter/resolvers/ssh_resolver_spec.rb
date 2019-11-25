8173def9e5c5c632f0f742644d94917c5b8e74a2
Campean Gheorghe Liviu <gigi.campean@puppet.com>
2019-11-25 15:43:06 +0200

    (FACT-2135) Attempt before going home.

--- spec/facter/resolvers/ssh_resolver_spec.rb
+++ spec/facter/resolvers/ssh_resolver_spec.rb
@@ -1,22 +1,22 @@
 # frozen_string_literal: true
 
 describe 'SSHResolver' do
-  @file_names = %w[ssh_host_rsa_key.pub ssh_host_dsa_key.pub ssh_host_ecdsa_key.pub ssh_host_ed25519_key.pub]
-  @file_paths = %w[/etc/ssh /usr/local/etc/ssh /etc /usr/local/etc /etc/opt/ssh]
   before do
-    @file_paths.each do | directory_path |
-      @file_names.each do |file_path|
-        file = join(directory_path,file_path)
+    @file_names = %w[ssh_host_rsa_key.pub ssh_host_dsa_key.pub ssh_host_ecdsa_key.pub ssh_host_ed25519_key.pub]
+    @file_paths = %w[/etc/ssh /usr/local/etc/ssh /etc /usr/local/etc /etc/opt/ssh]
+    @file_paths.each do |directory_path|
+      @file_names.each do |file_name|
+        file_address = directory_path + file_name
         allow(File).to receive(:directory?).with(directory_path).and_return(true)
-        allow(File).to receive(:file?).with(file_path).and_return(true)
-        allow(File).to receive(:read).with(file).and_return(file_content)
+        allow(File).to receive(:file?).with(file_name).and_return(true)
+        allow(File).to receive(:read).with(file_address).and_return(:file_address)
       end
     end
   end
   context '#resolve ssh facts' do
     it 'detects ruby sitedir' do
       result = Facter::Resolvers::SshResolver.resolve(:ssh)
-      expect(result).to eql(file_content)
+      expect(result).to eql(file_address)
     end
   end
 end
