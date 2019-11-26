47ded3a07c8ae09679f28903d397b7272c506fe0
Campean Gheorghe Liviu <gigi.campean@puppet.com>
2019-11-26 16:00:49 +0200

    (FACT-2135) Second attempt. Test passed, no good output yet though.

--- spec/facter/resolvers/ssh_resolver_spec.rb
+++ spec/facter/resolvers/ssh_resolver_spec.rb
@@ -4,19 +4,20 @@ describe 'SSHResolver' do
   before do
     @file_names = %w[ssh_host_rsa_key.pub ssh_host_dsa_key.pub ssh_host_ecdsa_key.pub ssh_host_ed25519_key.pub]
     @file_paths = %w[/etc/ssh /usr/local/etc/ssh /etc /usr/local/etc /etc/opt/ssh]
-    @file_paths.each do |directory_path|
-      @file_names.each do |file_name|
+    @file_paths.first do |directory_path|
+      @file_names.first do |file_name|
         file_address = directory_path + file_name
         allow(File).to receive(:directory?).with(directory_path).and_return(true)
-        allow(File).to receive(:file?).with(file_name).and_return(true)
-        allow(File).to receive(:read).with(file_address).and_return(:file_address)
+        allow(File).to receive(:file?).with(directory_path + file_name).and_return(true)
+        allow(File).to receive(:read).with(file_address).and_return(filecontent)
       end
     end
   end
   context '#resolve ssh facts' do
-    it 'detects ruby sitedir' do
+    let(:filecontent) { [] }
+    it 'detects ssh dir,file,fact' do
       result = Facter::Resolvers::SshResolver.resolve(:ssh)
-      expect(result).to eql(file_address)
+      expect(result).to eql(filecontent)
     end
   end
 end
