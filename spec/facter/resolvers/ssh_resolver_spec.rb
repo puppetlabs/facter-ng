f297f9198bc06b6add00cf6845fafefea7a5f703
Campean Gheorghe Liviu <gigi.campean@puppet.com>
2019-11-29 12:12:17 +0200

    (FACT-2135) Testing new attempt.

--- spec/facter/resolvers/ssh_resolver_spec.rb
+++ spec/facter/resolvers/ssh_resolver_spec.rb
@@ -4,8 +4,8 @@ describe 'SSHResolver' do
   before do
     @file_names = %w[ssh_host_rsa_key.pub ssh_host_dsa_key.pub ssh_host_ecdsa_key.pub ssh_host_ed25519_key.pub]
     @file_paths = %w[/etc/ssh /usr/local/etc/ssh /etc /usr/local/etc /etc/opt/ssh]
-    @file_paths.first do |directory_path|
-      @file_names.first do |file_name|
+    @file_paths.last do |directory_path|
+      @file_names.last do |file_name|
         file_address = directory_path + file_name
         allow(File).to receive(:directory?).with(directory_path).and_return(true)
         allow(File).to receive(:file?).with(file_address).and_return(true)
@@ -14,7 +14,7 @@ describe 'SSHResolver' do
     end
   end
   context '#resolve ssh facts' do
-    let(:filecontent) { [] }
+    let(:file_content) { [] }
     it 'detects ssh dir,file,fact' do
       result = Facter::Resolvers::SshResolver.resolve(:ssh)
       expect(result).to eql(file_content)
