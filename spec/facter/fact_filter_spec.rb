e7551d445919afa13c1d078242817957d9ee7cc5
Oana Tanasoiu <oana.tanasoiu@puppet.com>
2019-12-10 15:27:29 +0200

    (FACT-2178) Add test for FactFilter

--- spec/facter/fact_filter_spec.rb
+++ spec/facter/fact_filter_spec.rb
@@ -12,4 +12,16 @@ describe 'FactFilter' do
 
     expect(resolved_fact.value).to eq('18')
   end
+
+  it 'filters value inside fact when value is array' do
+    fact_value = { full: '18.7.0', major: '18', minor: 7, arry: ['val', { val2: 'val3' }] }
+    resolved_fact = Facter::ResolvedFact.new('os.release', fact_value)
+
+    resolved_fact.user_query = 'os.release.arry.1.val2'
+    resolved_fact.filter_tokens = %w[arry 1 val2]
+
+    Facter::FactFilter.new.filter_facts!([resolved_fact])
+
+    expect(resolved_fact.value).to eq('val3')
+  end
 end
