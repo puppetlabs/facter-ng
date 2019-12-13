e80d013884de1be51c74411c592767d891aa7d94
Bogdan Irimie <bogdan.irimie@puppet.com>
2019-12-13 18:52:16 +0200

    (FACT-2164) Fix rspec tests.

--- lib/framework/core/fact_loaders/class_discoverer.rb
+++ lib/framework/core/fact_loaders/class_discoverer.rb
@@ -14,7 +14,7 @@ module Facter
       # select only classes
       os_module_name.constants.select { |c| os_module_name.const_get(c).is_a? Class }
     rescue NameError
-       @log.error("There is no module named #{operating_system}")
+      @log.error("There is no module named #{operating_system}")
       []
     end
   end
