1a801aaf8ef33689569d634d005ee7981349b5e8
Campean Gheorghe Liviu <gigi.campean@puppet.com>
2019-12-16 14:38:52 +0200

    (FACT-2200) Attempting display of fact.

--- spec/facter/facts/macosx/dmi/manufacturer_spec.rb
+++ spec/facter/facts/macosx/dmi/manufacturer_spec.rb
@@ -3,9 +3,10 @@
 describe 'Macosx DmiManufacturer' do
   context '#call_the_resolver' do
     it 'returns a fact' do
-      expected_fact = double(Facter::ResolvedFact, name: 'dmi.manufacturer', value: 'value')
-      allow(Facter::Resolvers::Linux::DmiBios).to receive(:resolve).with(:sys_vendor).and_return('value')
-      allow(Facter::ResolvedFact).to receive(:new).with('dmi.manufacturer', 'value').and_return(expected_fact)
+      value = 'Anything that could work'
+      expected_fact = double(Facter::ResolvedFact, name: 'dmi.manufacturer', value: value)
+      allow(Facter::Resolvers::Linux::DmiBios).to receive(:resolve).with(:sys_vendor).and_return(value)
+      allow(Facter::ResolvedFact).to receive(:new).with('dmi.manufacturer', value).and_return(expected_fact)
 
       fact = Facter::Macosx::DmiManufacturer.new
       expect(fact.call_the_resolver).to eq(expected_fact)
