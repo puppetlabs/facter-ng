6b3dbb65b5b3ba250aacf3a9899657e447a1e9f0
Oana Tanasoiu <oana.tanasoiu@puppet.com>
2019-12-17 13:43:46 +0200

    (FACT-2178) Refactor specs

--- spec/facter/resolvers/windows/networking_resolver_spec.rb
+++ spec/facter/resolvers/windows/networking_resolver_spec.rb
@@ -29,12 +29,12 @@ describe 'Windows Networking Resolver' do
     context 'when fails to retrieve networking information after 3 tries' do
       let(:error_code) { NetworkingFFI::ERROR_BUFFER_OVERFLOW }
       before do
-        allow(FFI::MemoryPointer).to receive(:new).exactly(4).time
+        allow(FFI::MemoryPointer).to receive(:new).exactly(4).times
                                                   .with(IpAdapterAddressesLh.size, NetworkingFFI::BUFFER_LENGTH)
                                                   .and_return(adapter_address)
         allow(NetworkingFFI)
           .to receive(:GetAdaptersAddresses)
-          .exactly(3).time
+          .exactly(3).times
           .with(NetworkingFFI::AF_UNSPEC, 14, FFI::Pointer::NULL, adapter_address, size_ptr)
           .and_return(error_code)
       end
