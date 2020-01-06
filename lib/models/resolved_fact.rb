e044d5cc680eee809e547bb310533b15b2a27493
Oana Tanasoiu <oana.tanasoiu@puppet.com>
2020-01-06 11:43:23 +0200

    (FACT-2155) Resolve comments

--- lib/models/resolved_fact.rb
+++ lib/models/resolved_fact.rb
@@ -2,16 +2,21 @@
 
 module Facter
   class ResolvedFact
-
     attr_reader :name, :type
     attr_accessor :user_query, :filter_tokens, :value
 
     def initialize(name, value = '', type = :core)
       @name = name
       @value = value
-      logger = Log.new(self)
-      type =~ /core|legacy/ ? @type = type : logger.warn('The specified type is invalid!')
-      @type ||= :unknown
+      type =~ /core|legacy/ ? @type = type : (raise ArgumentError, 'The type provided for fact is not legacy or core!')
+    end
+
+    def legacy?
+      type == :legacy
+    end
+
+    def core?
+      type == :core
     end
   end
 end
