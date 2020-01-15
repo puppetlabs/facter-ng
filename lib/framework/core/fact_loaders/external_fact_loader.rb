b0d4d17c9cce3d6ba892b785c220d74dc59c14da
Bogdan Irimie <bogdan.irimie@puppet.com>
2020-01-15 16:46:29 +0200

    (FACT-2294) Only load external facts when they are requested, not when instantiating external fact loader.

--- lib/framework/core/fact_loaders/external_fact_loader.rb
+++ lib/framework/core/fact_loaders/external_fact_loader.rb
@@ -2,47 +2,49 @@
 
 module Facter
   class ExternalFactLoader
-    attr_reader :custom_facts, :external_facts, :facts
+    def custom_facts
+      @custom_facts.nil? ? load_custom_facts : @custom_facts
+    end
 
-    def initialize
-      @custom_facts = []
-      @external_facts = []
-      @facts = []
+    def external_facts
+      @external_facts.nil? ? load_external_facts : @external_facts
+    end
 
-      directories_to_search
-      load_custom_facts
-      load_external_facts
+    def all_facts
+      load_custom_facts if @custom_facts.nil?
+      load_external_facts if @external_facts.nil?
 
-      all_facts
+      @facts.nil? ? Utils.deep_copy(@custom_facts).concat(@external_facts) : @facts
     end
 
     private
 
-    def all_facts
-      @facts = Utils.deep_copy(@custom_facts).concat(@external_facts)
-    end
-
-    def directories_to_search
-      LegacyFacter.search(*Options.custom_dir) if Options.custom_dir?
-      LegacyFacter.search_external(Options.external_dir) if Options.external_dir?
-    end
-
     def load_custom_facts
+      @custom_facts = []
+
+      LegacyFacter.search(*Options.custom_dir) if Options.custom_dir?
       custom_facts_to_load = LegacyFacter.collection.custom_facts
 
       custom_facts_to_load&.each do |custom_fact_name|
         loaded_fact = LoadedFact.new(custom_fact_name.to_s, nil, :custom)
         @custom_facts << loaded_fact
       end
+
+      @custom_facts
     end
 
     def load_external_facts
+      @external_facts = []
+
+      LegacyFacter.search_external(Options.external_dir) if Options.external_dir?
       external_facts_to_load = LegacyFacter.collection.external_facts
 
       external_facts_to_load&.each do |external_fact_name|
         loaded_fact = LoadedFact.new(external_fact_name.to_s, nil, :external)
         @external_facts << loaded_fact
       end
+
+      @external_facts
     end
   end
 end
