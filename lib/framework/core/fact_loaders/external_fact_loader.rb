7fe213d5a565f57285c683753941454f97735170
Bogdan Irimie <bogdan.irimie@puppet.com>
2020-01-15 17:32:17 +0200

    (FACT-2294) PR review changes.

--- lib/framework/core/fact_loaders/external_fact_loader.rb
+++ lib/framework/core/fact_loaders/external_fact_loader.rb
@@ -11,40 +11,37 @@ module Facter
     end
 
     def all_facts
-      load_custom_facts if @custom_facts.nil?
-      load_external_facts if @external_facts.nil?
-
-      @facts.nil? ? Utils.deep_copy(@custom_facts).concat(Utils.deep_copy(@external_facts)) : @facts
+      @all_facts ||= Utils.deep_copy(custom_facts + external_facts)
     end
 
     private
 
     def load_custom_facts
-      @custom_facts = []
+      custom_facts = []
 
       LegacyFacter.search(*Options.custom_dir) if Options.custom_dir?
       custom_facts_to_load = LegacyFacter.collection.custom_facts
 
       custom_facts_to_load&.each do |custom_fact_name|
         loaded_fact = LoadedFact.new(custom_fact_name.to_s, nil, :custom)
-        @custom_facts << loaded_fact
+        custom_facts << loaded_fact
       end
 
-      @custom_facts
+      custom_facts
     end
 
     def load_external_facts
-      @external_facts = []
+      external_facts = []
 
       LegacyFacter.search_external(Options.external_dir) if Options.external_dir?
       external_facts_to_load = LegacyFacter.collection.external_facts
 
       external_facts_to_load&.each do |external_fact_name|
         loaded_fact = LoadedFact.new(external_fact_name.to_s, nil, :external)
-        @external_facts << loaded_fact
+        external_facts << loaded_fact
       end
 
-      @external_facts
+      external_facts
     end
   end
 end
