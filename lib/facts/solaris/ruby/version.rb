04500d718a974a6e8b77d9325ddff50978c59910
Bogdan Irimie <bogdan.irimie@puppet.com>
2020-01-20 17:22:10 +0200

    (FACT-2287) Add ruby.version, ruby.platform, ruby.sitedir and  legacy fact rubyversion for Solaris.

---
+++ lib/facts/solaris/ruby/version.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Facter
+  module Solaris
+    class RubyVersion
+      FACT_NAME = 'ruby.version'
+      ALIASES = 'rubyversion'
+
+      def call_the_resolver
+        fact_value = Resolvers::Ruby.resolve(:version)
+        [ResolvedFact.new(FACT_NAME, fact_value), ResolvedFact.new(ALIASES, fact_value, :legacy)]
+      end
+    end
+  end
+end
---
+++ spec/facter/facts/solaris/ruby/version_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+describe 'Solaris RubyVersion' do
+  context '#call_the_resolver' do
+    let(:value) { '2.4.5' }
+    subject(:fact) { Facter::Solaris::RubyVersion.new }
+
+    before do
+      allow(Facter::Resolvers::Ruby).to receive(:resolve).with(:version).and_return(value)
+    end
+
+    it 'calls Facter::Resolvers::Ruby' do
+      expect(Facter::Resolvers::Ruby).to receive(:resolve).with(:version)
+      fact.call_the_resolver
+    end
+
+    it 'returns ruby version fact' do
+      expect(fact.call_the_resolver)
+        .to be_an_instance_of(Array)
+        .and contain_exactly(an_object_having_attributes(name: 'ruby.version', value: value),
+                             an_object_having_attributes(name: 'rubyversion', value: value, type: :legacy))
+    end
+  end
+end
