fc5d5f3360e724d10cf110fcb356bfe52e247ece
Bogdan Irimie <bogdan.irimie@puppet.com>
2020-02-04 09:50:19 +0200

    (FACT-2343) Fix query on arrays.

--- lib/framework/formatters/formatter_helper.rb
+++ lib/framework/formatters/formatter_helper.rb
@@ -8,7 +8,7 @@ module Facter
         user_queries.each do |user_query|
           fact_collection = build_fact_collection_for_user_query(user_query, resolved_facts)
 
-          printable_value = fact_collection.dig(*user_query.split('.'))
+          printable_value = fact_collection.dig(*split_user_query(user_query))
           facts_to_display.merge!(user_query => printable_value)
         end
 
@@ -23,7 +23,7 @@ module Facter
       def retrieve_fact_value_for_single_query(user_query, resolved_facts)
         fact_collection = build_fact_collection_for_user_query(user_query, resolved_facts)
         fact_collection = Facter::Utils.sort_hash_by_key(fact_collection)
-        fact_collection.dig(*user_query.split('.'))
+        fact_collection.dig(*split_user_query(user_query))
       end
 
       private
@@ -31,6 +31,11 @@ module Facter
       def build_fact_collection_for_user_query(user_query, resolved_facts)
         facts_for_query = resolved_facts.select { |resolved_fact| resolved_fact.user_query == user_query }
         FactCollection.new.build_fact_collection!(facts_for_query)
+      end
+
+      def split_user_query(user_query)
+        queries = user_query.split('.')
+        queries.map!{|query| query =~ /^[0-9]+$/ ? query.to_i : query}
       end
     end
   end
