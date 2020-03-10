843e0e3ae6592be825ad934ce8361c1a4f929f4a
Andrei Filipovici <andrei.filipovici@andreis-mbp.eth.tsr.corp.puppet.net>
2020-03-10 12:06:35 +0200

    (FACT-2444) Added alias for bios version and vendor on Debian

--- spec/facter/facts/debian/dmi/bios/release_date_spec.rb
+++ spec/facter/facts/debian/dmi/bios/release_date_spec.rb
@@ -18,8 +18,8 @@ describe Facts::Debian::Dmi::Bios::ReleaseDate do
 
     it 'returns a resolved fact' do
       expect(fact.call_the_resolver).to be_an_instance_of(Array).and \
-        contain_exactly(an_object_having_attributes(name: 'dmi.bios.release_date', value: value),
-                        an_object_having_attributes(name: 'bios_release_date', value: value, type: :legacy))
+        contain_exactly(an_object_having_attributes(name: 'dmi.bios.release_date', value: date),
+                        an_object_having_attributes(name: 'bios_release_date', value: date, type: :legacy))
     end
   end
 end
