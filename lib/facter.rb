f8d95480126adce961819fb8af6734d71d7369a3
Sebastian Miclea <sebastian.miclea@puppet.com>
2020-03-16 19:08:45 +0200

    ApiDebugger tool

--- lib/facter.rb
+++ lib/facter.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'pathname'
+require_relative 'util/api_debugger' if ENV['API_DEBUG']
 
 ROOT_DIR = Pathname.new(File.expand_path('..', __dir__)) unless defined?(ROOT_DIR)
 
@@ -349,5 +350,7 @@ module Facter
       )
       nil
     end
+
+    prepend ApiDebugger if ENV['API_DEBUG']
   end
 end
