8d6ee47afbc221ad59a3be982555dbcf8d721ae7
Bogdan Irimie <bogdan.irimie@puppet.com>
2020-03-20 16:06:29 +0200

    (FACT-2478) Add unit tests.

--- lib/custom_facts/util/collection.rb
+++ lib/custom_facts/util/collection.rb
@@ -116,7 +116,7 @@ module LegacyFacter
         internal_loader.load_all unless @loaded
         @loaded = true
 
-        custom_facts = @facts.select { |_k, v| v.options[:fact_type] }
+        custom_facts = @facts.select { |_k, v| v.options[:fact_type] == :custom }
         @custom_facts = Facter::Utils.deep_copy(custom_facts.keys)
       end
 
