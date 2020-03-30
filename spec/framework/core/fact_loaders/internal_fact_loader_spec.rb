cb08d9c6d4621f2ebaab6aedfb1a7b74e3a19f2f
Bogdan Irimie <bogdan.irimie@puppet.com>
2020-03-30 14:18:41 +0300

    (FACT-2498) Refactor tests to be compliant with rspec best practices.

--- spec/framework/core/fact_loaders/internal_fact_loader_spec.rb
+++ spec/framework/core/fact_loaders/internal_fact_loader_spec.rb
@@ -1,20 +1,20 @@
 # frozen_string_literal: true
 
 describe Facter::InternalFactLoader do
-  before do
-    os_detector_mock = instance_double(OsDetector)
-    allow(os_detector_mock).to receive(:hierarchy).and_return([:Debian])
-    allow(OsDetector).to receive(:instance).and_return(os_detector_mock)
-  end
-
   describe '#initialize' do
-    context 'when loading facts' do
-      it 'loads one legacy fact' do
-        os_detector_mock = instance_double(OsDetector)
+    let(:os_detector_mock) { instance_spy(OsDetector) }
+
+    before do
+      allow(os_detector_mock).to receive(:hierarchy).and_return([:Debian])
+      allow(OsDetector).to receive(:instance).and_return(os_detector_mock)
+    end
+
+    context 'when loading one legacy facts' do
+      before do
         allow(os_detector_mock).to receive(:hierarchy).and_return([:Windows])
         allow(OsDetector).to receive(:instance).and_return(os_detector_mock)
 
-        class_discoverer_mock = instance_double(Facter::ClassDiscoverer)
+        class_discoverer_mock = instance_spy(Facter::ClassDiscoverer)
         allow(class_discoverer_mock)
           .to receive(:discover_classes)
           .with(:Windows)
@@ -22,39 +22,64 @@ describe Facter::InternalFactLoader do
         allow(Facter::ClassDiscoverer).to receive(:instance).and_return(class_discoverer_mock)
 
         stub_const('Facts::Windows::NetworkInterfaces::FACT_NAME', 'network_.*')
-
-        internal_fact_loader = Facter::InternalFactLoader.new
-        legacy_facts = internal_fact_loader.legacy_facts
-        core_facts = internal_fact_loader.core_facts
-
-        expect(legacy_facts.size).to eq(1)
-        expect(core_facts.size).to eq(0)
-        expect(legacy_facts.first.type).to eq(:legacy)
       end
 
-      it 'loads one core fact' do
-        class_discoverer_mock = instance_double(Facter::ClassDiscoverer)
+      it 'loads no core facts' do
+        internal_fact_loader = Facter::InternalFactLoader.new
+
+        expect(internal_fact_loader.core_facts).to be_empty
+      end
+
+      it 'loads one legacy fact' do
+        internal_fact_loader = Facter::InternalFactLoader.new
+
+        expect(internal_fact_loader.legacy_facts.size).to eq(1)
+      end
+
+      it 'loads one fact with :legacy type' do
+        internal_fact_loader = Facter::InternalFactLoader.new
+
+        expect(internal_fact_loader.legacy_facts.first.type).to eq(:legacy)
+      end
+    end
+
+    context 'when loading one core fact' do
+      before do
+        class_discoverer_mock = instance_spy(Facter::ClassDiscoverer)
         allow(class_discoverer_mock)
           .to receive(:discover_classes)
           .with(:Debian)
-          .and_return([Facts::Debian::Os::Name])
+          .and_return([Facts::Debian::Path])
         allow(Facter::ClassDiscoverer).to receive(:instance).and_return(class_discoverer_mock)
 
-        stub_const('Facts::Debian::OsName::FACT_NAME', 'os.name')
-
-        internal_fact_loader = Facter::InternalFactLoader.new
-        core_facts = internal_fact_loader.core_facts
-
-        expect(core_facts.size).to eq(1)
-        expect(core_facts.first.type).to eq(:core)
+        stub_const('Facts::Debian::Path::FACT_NAME', 'path')
       end
 
-      it 'loads one legacy fact and one core fact' do
-        os_detector_mock = instance_double(OsDetector)
+      it 'loads no legacy facts' do
+        internal_fact_loader = Facter::InternalFactLoader.new
+
+        expect(internal_fact_loader.legacy_facts).to be_empty
+      end
+
+      it 'loads one core fact' do
+        internal_fact_loader = Facter::InternalFactLoader.new
+
+        expect(internal_fact_loader.core_facts.size).to eq(1)
+      end
+
+      it 'loads one fact with :core type' do
+        internal_fact_loader = Facter::InternalFactLoader.new
+
+        expect(internal_fact_loader.core_facts.first.type).to eq(:core)
+      end
+    end
+
+    context 'when loading one legacy fact and one core fact' do
+      before do
         allow(os_detector_mock).to receive(:hierarchy).and_return([:Windows])
         allow(OsDetector).to receive(:instance).and_return(os_detector_mock)
 
-        class_discoverer_mock = instance_double(Facter::ClassDiscoverer)
+        class_discoverer_mock = instance_spy(Facter::ClassDiscoverer)
         allow(class_discoverer_mock)
           .to receive(:discover_classes)
           .with(:Windows)
@@ -63,37 +88,50 @@ describe Facter::InternalFactLoader do
 
         stub_const('Facts::Windows::NetworkInterface::FACT_NAME', 'network_.*')
         stub_const('Facts::Windows::OsName::FACT_NAME', 'path')
-
-        internal_fact_loader = Facter::InternalFactLoader.new
-        all_facts = internal_fact_loader.facts
-
-        expect(all_facts.size).to eq(2)
-        expect(all_facts.find { |loaded_fact| loaded_fact.type == :core }.type).to eq(:core)
-        expect(all_facts.find { |loaded_fact| loaded_fact.type == :legacy }.type).to eq(:legacy)
       end
 
-      it 'loads no facts' do
-        class_discoverer_mock = instance_double(Facter::ClassDiscoverer)
+      it 'loads two fact' do
+        internal_fact_loader = Facter::InternalFactLoader.new
+
+        expect(internal_fact_loader.facts.size).to eq(2)
+      end
+
+      it 'loads one legacy fact' do
+        internal_fact_loader = Facter::InternalFactLoader.new
+
+        expect(internal_fact_loader.facts.count { |lf| lf.type == :legacy }).to eq(1)
+      end
+
+      it 'loads one core fact' do
+        internal_fact_loader = Facter::InternalFactLoader.new
+
+        expect(internal_fact_loader.facts.count { |lf| lf.type == :core }).to eq(1)
+      end
+    end
+
+    context 'when loading no facts' do
+      before do
+        class_discoverer_mock = instance_spy(Facter::ClassDiscoverer)
         allow(class_discoverer_mock)
           .to receive(:discover_classes)
           .with(:Debian)
           .and_return([])
         allow(Facter::ClassDiscoverer).to receive(:instance).and_return(class_discoverer_mock)
+      end
 
+      it 'loads no facts' do
         internal_fact_loader = Facter::InternalFactLoader.new
-        all_facts_hash = internal_fact_loader.facts
 
-        expect(all_facts_hash.size).to eq(0)
+        expect(internal_fact_loader.facts).to be_empty
       end
     end
 
     context 'when loading hierarchy of facts' do
       before do
-        os_detector_mock = instance_double(OsDetector)
         allow(os_detector_mock).to receive(:hierarchy).and_return(%i[Debian El])
         allow(OsDetector).to receive(:instance).and_return(os_detector_mock)
 
-        class_discoverer_mock = instance_double(Facter::ClassDiscoverer)
+        class_discoverer_mock = instance_spy(Facter::ClassDiscoverer)
         allow(class_discoverer_mock)
           .to receive(:discover_classes)
           .with(:Debian)
