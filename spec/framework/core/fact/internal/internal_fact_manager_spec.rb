93648eb3c47eb2e1a8bfd7f7f1fce03b593f65ef
Bogdan Irimie <bogdan.irimie@puppet.com>
2020-04-01 12:17:34 +0300

    (FACT-2499) Only resolve fact once. Fix test.

--- spec/framework/core/fact/internal/internal_fact_manager_spec.rb
+++ spec/framework/core/fact/internal/internal_fact_manager_spec.rb
@@ -3,15 +3,16 @@
 describe Facter::InternalFactManager do
   describe '#resolve_facts' do
     it 'resolved one core fact' do
-      ubuntu_os_name = double(Facts::Debian::Os::Name)
+      os_name_class_spy = class_spy(Facts::Debian::Os::Name)
+      os_name_instance_spy = instance_spy(Facts::Debian::Os::Name)
 
       resolved_fact = mock_resolved_fact('os', 'Ubuntu', nil, [])
 
-      allow(ubuntu_os_name).to receive(:new).and_return(ubuntu_os_name)
-      allow(ubuntu_os_name).to receive(:call_the_resolver).and_return(resolved_fact)
+      allow(os_name_class_spy).to receive(:new).and_return(os_name_instance_spy)
+      allow(os_name_instance_spy).to receive(:call_the_resolver).and_return(resolved_fact)
 
-      searched_fact = double(Facter::SearchedFact, name: 'os', fact_class: ubuntu_os_name, filter_tokens: [],
-                                                   user_query: '', type: :core)
+      searched_fact = instance_spy(Facter::SearchedFact, name: 'os', fact_class: os_name_class_spy, filter_tokens: [],
+                                                         user_query: '', type: :core)
 
       core_fact_manager = Facter::InternalFactManager.new
       resolved_facts = core_fact_manager.resolve_facts([searched_fact])
@@ -20,15 +21,16 @@ describe Facter::InternalFactManager do
     end
 
     it 'resolved one legacy fact' do
-      windows_networking_interface = double(Facts::Windows::NetworkInterfaces)
+      networking_interface_class_spy = class_spy(Facts::Windows::NetworkInterfaces)
+      windows_networking_interface = instance_spy(Facts::Windows::NetworkInterfaces)
 
       resolved_fact = mock_resolved_fact('network_Ethernet0', '192.168.5.121', nil, [], :legacy)
 
-      allow(windows_networking_interface).to receive(:new).and_return(windows_networking_interface)
+      allow(networking_interface_class_spy).to receive(:new).and_return(windows_networking_interface)
       allow(windows_networking_interface).to receive(:call_the_resolver).and_return(resolved_fact)
 
-      searched_fact = double(Facter::SearchedFact, name: 'network_.*', fact_class: windows_networking_interface,
-                                                   filter_tokens: [], user_query: '', type: :core)
+      searched_fact = instance_spy(Facter::SearchedFact, name: 'network_.*', fact_class: networking_interface_class_spy,
+                                                         filter_tokens: [], user_query: '', type: :core)
 
       core_fact_manager = Facter::InternalFactManager.new
       resolved_facts = core_fact_manager.resolve_facts([searched_fact])
