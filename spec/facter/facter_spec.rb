1b120677229284b945d09863ddcf784ff09c2f83
Bogdan Irimie <bogdan.irimie@puppet.com>
2020-04-21 10:43:59 +0300

    (FACT-2562) Update tests description.

--- spec/facter/facter_spec.rb
+++ spec/facter/facter_spec.rb
@@ -285,7 +285,7 @@ describe Facter do
   end
 
   describe '#search_path' do
-    it 'sends call to LegacyFacter' do
+    it 'sends call to Facter::Options' do
       expect(Facter::Options).to receive(:custom_dir).once
       Facter.search_path
     end
@@ -301,7 +301,7 @@ describe Facter do
   end
 
   describe '#search_external_path' do
-    it 'sends call to LegacyFacter' do
+    it 'sends call to Facter::Options' do
       expect(Facter::Options).to receive(:external_dir).once
       Facter.search_external_path
     end
