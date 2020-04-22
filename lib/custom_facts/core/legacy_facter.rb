d4c12da901537a98a3c624f4d32acc3b1cdde115
Bogdan Irimie <bogdan.irimie@puppet.com>
2020-04-22 17:18:09 +0300

    (FACT-2570) Remove commented code.

--- lib/custom_facts/core/legacy_facter.rb
+++ lib/custom_facts/core/legacy_facter.rb
@@ -206,44 +206,4 @@ module LegacyFacter
   def self.loadfacts
     collection.load_all
   end
-
-  # Register directories to be searched for facts. The registered directories
-  # must be absolute paths or they will be ignored.
-  #
-  # @param dirs [String] directories to search
-  #
-  # @return [void]
-  #
-  # @api public
-  # def self.search(*dirs)
-  #   @search_path += dirs
-  # end
-
-  # Returns the registered search directories.
-  #
-  # @return [Array<String>] An array of the directories searched
-  #
-  # @api public
-  # def self.search_path
-  #   @search_path.dup
-  # end
-
-  # Reset the Facter search directories.
-  #
-  # @api private
-  # @return [void]
-  # def self.reset_search_path!
-  #   @search_path = []
-  # end
-
-  # reset_search_path!
-
-  # Returns the registered search directories.
-  #
-  # @return [Array<String>] An array of the directories searched
-  #
-  # @api public
-  # def self.search_external_path
-  #   LegacyFacter::Util::Config.external_facts_dirs.dup
-  # end
 end
