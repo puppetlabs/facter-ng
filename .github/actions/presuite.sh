8bd4c52797a608fa06c88b91581999aa7e83afe6
Andrei Filipovici <andrei.filipovici@andreis-mbp.eth.tsr.corp.puppet.net>
2020-04-23 18:08:24 +0300

    (FACT-2569) add install bundler and facter 4 dependencies

--- .github/actions/presuite.sh
+++ .github/actions/presuite.sh
@@ -1,10 +1,18 @@
 #!/bin/sh -x
 
-echo $BP_ROOT
-echo $FACTER_4_ROOT
-
 export SHA=`curl --fail --silent GET --url http://builds.delivery.puppetlabs.net/passing-agent-SHAs/puppet-agent-master`
-echo 'Puppet Agent SHA is ' $SHA
+echo 'Puppet Agent SHA is' `curl --fail --silent GET --url http://builds.delivery.puppetlabs.net/passing-agent-SHAs/puppet-agent-master`
+
+echo 'Install bundler'
+gem install bundler
+
+echo 'Install beaker'
+gem install beaker
+
+where beaker
+echo $PATH
+echo 'Install facter 4 dependencies'
+cd $FACTER_4_ROOT && bundle install
 
 echo 'Starting pre-suite'
 bundle exec beaker exec pre-suite --pre-suite $BP_ROOT/setup/common/000-delete-puppet-when-none.rb,$BP_ROOT/setup/aio/010_Install_Puppet_Agent.rb
@@ -14,5 +22,7 @@ echo 'Configure facter 4 as facter 3'
 puppet config set facterng true
 
 gem build $FACTER_4_ROOT/facter.gemspec
+
+ls -la
 
 gem install -f facter-*.gem
