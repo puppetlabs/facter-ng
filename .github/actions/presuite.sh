5c6fbdcd2f1d72665740a26c96044edab584465f
Andrei Filipovici <andrei.filipovici@andreis-mbp.eth.tsr.corp.puppet.net>
2020-04-27 17:34:01 +0300

    debugger

--- .github/actions/presuite.sh
+++ .github/actions/presuite.sh
@@ -1,25 +1,9 @@
 #!/bin/sh -x
 
-cwd=$(pwd)
-#sudo apt-get install make gcc ruby-dev
-echo '---------------'
-#sudo su
-echo '---------------'
-
-#id -a
-
 export DEBIAN_DISABLE_RUBYGEMS_INTEGRATION=salam
-export PATH=/opt/puppetlabs/puppet/bin:/opt/puppetlabs/bin:/usr/local/bin:/home/runner/work/facter-ng/facter-ng/facter_3/acceptance/vendor/bundle/ruby/2.5.0/bin/:$PATH
-#sudo -E sh -c 'env'
-
-#sudo sh -c "echo 'Defaults exempt_group = docker' >> /etc/sudoers"
-echo '-----------'
-#sudo cat /etc/sudoers
-
-#sudo -E sh -c 'env'
+cwd=$(pwd)
 
 printf '\nInstall bundler\n'
-
 gem install bundler
 git config --global http.sslVerify false
 bundle config set git.allow_insecure true
@@ -28,18 +12,18 @@ cd $cwd/$BEAKER_ROOT
 gem build beaker.gemspec
 
 printf '\nInstall facter 3 dependencies\n'
-mkdir -p /opt/puppetlabs/puppet/lib/ruby/gems/2.5.0
-export GEM_HOME=/opt/puppetlabs/puppet/lib/ruby/gems/2.5.0
 cd $cwd/$FACTER_3_ROOT/acceptance && bundle install
 
+printf '\nInstall custom beaker\n'
 gem install $cwd/$BEAKER_ROOT/beaker-*.gem --bindir /bin
 bundle info beaker --path
 
-BP_ROOT=`bundle info beaker-puppet --path`
+printf '\nBeaker provision\n'
 beaker init -h ubuntu1804-64a{hypervisor=none\,hostname=localhost} -o config/aio/options.rb
 beaker provision
 
-printf '\nStarting pre-suite\n'
+printf '\nBeaker pre-suite\n'
+BP_ROOT=`bundle info beaker-puppet --path`
 beaker exec pre-suite --pre-suite $BP_ROOT/setup/aio/010_Install_Puppet_Agent.rb
 
 #echo '\nInstall facter 4 dependencies'
@@ -53,5 +37,5 @@ beaker exec pre-suite --pre-suite $BP_ROOT/setup/aio/010_Install_Puppet_Agent.rb
 
 #cd $cwd/$FACTER_3_ROOT/acceptance
 
-printf '\nRunning tests\n'
+printf '\nBeaker tests\n'
 beaker exec tests
