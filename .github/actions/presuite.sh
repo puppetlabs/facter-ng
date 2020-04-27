8379d7c3bbfaf96a2aa523767c2894a6706faa89
Andrei Filipovici <andrei.filipovici@andreis-mbp.eth.tsr.corp.puppet.net>
2020-04-27 17:55:05 +0300

    run tests on facter 4

--- .github/actions/presuite.sh
+++ .github/actions/presuite.sh
@@ -15,6 +15,15 @@ printf '\nInstall custom beaker\n'
 gem install $cwd/$BEAKER_ROOT/beaker-*.gem --bindir /bin
 bundle info beaker --path
 
+
+printf '\nBeaker provision\n'
+beaker init -h ubuntu1804-64a{hypervisor=none\,hostname=localhost} -o config/aio/options.rb
+beaker provision
+
+printf '\nBeaker pre-suite\n'
+BP_ROOT=`bundle info beaker-puppet --path`
+beaker exec pre-suite --pre-suite $BP_ROOT/setup/aio/010_Install_Puppet_Agent.rb
+
 echo '\nInstall facter 4 dependencies'
 cd $cwd/$FACTER_4_ROOT && bundle install
 
@@ -25,14 +34,6 @@ gem build facter.gemspec
 gem install -f facter-*.gem
 
 cd $cwd/$FACTER_3_ROOT/acceptance
-
-printf '\nBeaker provision\n'
-beaker init -h ubuntu1804-64a{hypervisor=none\,hostname=localhost} -o config/aio/options.rb
-beaker provision
-
-printf '\nBeaker pre-suite\n'
-BP_ROOT=`bundle info beaker-puppet --path`
-beaker exec pre-suite --pre-suite $BP_ROOT/setup/aio/010_Install_Puppet_Agent.rb
 
 printf '\nBeaker tests\n'
 beaker exec tests
