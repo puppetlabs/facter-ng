0f2ab597344c10e8602aa2a853a5a0688cb56230
Andrei Filipovici <andrei.filipovici@andreis-mbp.eth.tsr.corp.puppet.net>
2020-04-27 18:36:35 +0300

    run tests on facter 4

--- .github/actions/presuite.sh
+++ .github/actions/presuite.sh
@@ -3,38 +3,37 @@
 export DEBIAN_DISABLE_RUBYGEMS_INTEGRATION=nothing
 cwd=$(pwd)
 
-printf '\nInstall bundler\n'
+printf '\nInstall bundler\n\n'
 gem install bundler
 
-cd $cwd/$BEAKER_ROOT
-gem build beaker.gemspec
-
-printf '\nInstall facter 3 dependencies\n'
+printf '\nInstall facter 3 dependencies\n\n'
 cd $cwd/$FACTER_3_ROOT/acceptance && bundle install
 
-printf '\nInstall custom beaker\n'
-gem install $cwd/$BEAKER_ROOT/beaker-*.gem --bindir /bin
+printf '\nInstall custom beaker\n\n'
+cd $cwd/$BEAKER_ROOT
+gem build beaker.gemspec
+gem install beaker-*.gem --bindir /bin
 bundle info beaker --path
 
-
-printf '\nBeaker provision\n'
+printf '\nBeaker provision\n\n'
+cd $cwd/$FACTER_3_ROOT/acceptance
 beaker init -h ubuntu1804-64a{hypervisor=none\,hostname=localhost} -o config/aio/options.rb
 beaker provision
 
-printf '\nBeaker pre-suite\n'
+printf '\nBeaker pre-suite\n\n'
 BP_ROOT=`bundle info beaker-puppet --path`
 beaker exec pre-suite --pre-suite $BP_ROOT/setup/aio/010_Install_Puppet_Agent.rb
 
-printf '\nConfigure facter 4 as facter 3'
+printf '\nConfigure facter 4 as facter 3\n\n'
 puppet config set facterng true
 
-printf '\nInstall facter 4 dependencies'
+printf '\nInstall facter 4 dependencies\n\n'
 cd $cwd/$FACTER_4_ROOT && bundle install
 
+printf '\nInstall facter 4\n\n'
 gem build facter.gemspec
 gem install -f facter-*.gem
 
+printf '\nBeaker tests\n\n'
 cd $cwd/$FACTER_3_ROOT/acceptance
-
-printf '\nBeaker tests\n'
 beaker exec tests
