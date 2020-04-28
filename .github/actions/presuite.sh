f693a6e2c04aab5e4a715409aeb08da0c4df8b93
Andrei Filipovici <andrei.filipovici@andreis-mbp.eth.tsr.corp.puppet.net>
2020-04-28 16:46:55 +0300

    removed test results parsing

--- .github/actions/presuite.sh
+++ .github/actions/presuite.sh
@@ -37,5 +37,4 @@ gem install -f facter-*.gem
 
 printf '\nBeaker tests\n\n'
 cd $cwd/$FACTER_3_ROOT/acceptance
-beaker exec tests --test-tag-exclude=server,facter_3 --test-tag-or=risk:high,audit:high 2>&1 | tee results.txt
-sed -n '/Failed Tests Cases:/,/Skipped Tests Cases:/p' results.txt | grep 'Test Case' | awk {'print $3'}
+beaker exec tests --test-tag-exclude=server,facter_3 --test-tag-or=risk:high,audit:high
