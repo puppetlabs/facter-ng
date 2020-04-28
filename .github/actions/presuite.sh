8c81c5628de0d2490a8eeb57228480e34c808abc
Andrei Filipovici <andrei.filipovici@andreis-mbp.eth.tsr.corp.puppet.net>
2020-04-28 19:47:58 +0300

    (FACT-2569) changed ruby setup

--- .github/actions/presuite.sh
+++ .github/actions/presuite.sh
@@ -1,21 +1,22 @@
 #!/bin/sh -x
 
 export DEBIAN_DISABLE_RUBYGEMS_INTEGRATION=no_wornings
-export DENABLE_PATH_CHECK=0
 export PATH=/opt/puppetlabs/puppet/bin/:/opt/puppetlabs/bin:$PATH
 cwd=$(pwd)
 
 printf '\nInstall bundler\n\n'
 gem install bundler
 
-printf '\nInstall facter 3 dependencies\n\n'
+printf '\nInstall facter 4 dependencies\n\n'
+cd $cwd/$FACTER_4_ROOT && bundle install
+
+printf '\nInstall facter 3 acceptance dependencies\n\n'
 cd $cwd/$FACTER_3_ROOT/acceptance && bundle install
 
 printf '\nInstall custom beaker\n\n'
 cd $cwd/$BEAKER_ROOT
 gem build beaker.gemspec
 gem install beaker-*.gem --bindir /bin
-bundle info beaker --path
 
 printf '\nBeaker provision\n\n'
 cd $cwd/$FACTER_3_ROOT/acceptance
@@ -29,15 +30,15 @@ beaker exec pre-suite --pre-suite $BP_ROOT/setup/aio/010_Install_Puppet_Agent.rb
 printf '\nConfigure facter 4 as facter 3\n\n'
 puppet config set facterng true
 
-printf '\nInstall facter 4 dependencies\n\n'
-cd $cwd/$FACTER_4_ROOT && bundle install
+agent_facter_ng_version=`facter-ng --version | tr -d '\r'`
 
-printf '\nInstall facter 4\n\n'
-gem build facter.gemspec
-gem install -f facter-*.gem
+cp -r $cwd/$FACTER_4_ROOT/* /opt/puppetlabs/puppet/lib/ruby/gems/2.5.0/gems/facter-ng-$agent_facter_ng_version/
+mv /opt/puppetlabs/puppet/bin/facter-ng /opt/puppetlabs/bin/facter
+
+puts facter -version
+puts puppet facts | grep facterversion
 
 printf '\nBeaker tests\n\n'
 cd $cwd/$FACTER_3_ROOT/acceptance
-
-beaker exec tests/external_facts/external_fact_stderr_messages_output_to_stderr.rb --test-tag-exclude=server,facter_3 --test-tag-or=risk:high,audit:high
-echo $PATH
+beaker exec tests --test-tag-exclude=server,facter_3 --test-tag-or=risk:high,audit:high 2>&1 | tee results.txt
+sed -n '/Failed Tests Cases:/,/Skipped Tests Cases:/p' results.txt | grep 'Test Case' | awk {'print $3'}
