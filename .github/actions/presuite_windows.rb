404b550f41567db6fff39c6660a28ed5761cde86
Andrei Filipovici <andrei.filipovici@andreis-mbp.eth.tsr.corp.puppet.net>
2020-05-13 17:35:51 +0300

    (FACT-2600) debug

--- .github/actions/presuite_windows.rb
+++ .github/actions/presuite_windows.rb
@@ -59,39 +59,44 @@ def install_puppet_agent
   message('INSTALL PUPPET AGENT')
 
   beaker_puppet_root, _ = run('bundle info beaker-puppet --path')
-  presuite_file_path = [File.join(beaker_puppet_root.chomp, 'setup', 'aio', '010_Install_Puppet_Agent.rb')]
+  presuite_file_paths = [File.join(beaker_puppet_root.chomp, 'setup', 'aio', '010_Install_Puppet_Agent.rb')]
 
-  env = { 'PATH' => "#{puppet_bin_dir};#{ENV['PATH']}" }
-  run("beaker exec pre-suite --pre-suite #{presuite_file_path.join(',')} --preserve-state", './', env)
+  unless HOST_PLATFORM.to_s.include? 'windows'
+    path_tokens = ['setup', 'common']
+    presuite_files = ['012_Finalize_Installs.rb', '025_StopFirewall.rb', '030_StopSssd.rb']
+
+    presuite_files.each do |file|
+      presuite_file_paths << File.join(beaker_puppet_root.chomp, path_tokens, file)
+    end
+  end
+
+  run("beaker exec pre-suite --pre-suite #{presuite_file_paths.join(',')} --preserve-state")
 end
 
 def replace_facter_3_with_facter_4
+  gem_command = File.join(puppet_bin_dir, 'gem')
   puppet_command = File.join(puppet_bin_dir, 'puppet')
 
   message('SET FACTER 4 FLAG TO TRUE')
-  run("#{puppet_command} config set facterng true")
+  run("'#{puppet_bin_dir}\\puppet' config set facterng true")
 
-  install_latest_facter_4(gem_command)
+  install_latest_facter_4("'C:\\Program Files\\Puppet Labs\\Puppet\\puppet\\bin\\gem'")
 
   message('CHANGE FACTER 3 WITH FACTER 4')
-  run('mv "facter-ng" "facter"', puppet_bin_dir)
+  puts puppet_bin_dir
+  run('mv "facter-ng.bat" "facter.bat"', puppet_bin_dir)
 end
 
 def puppet_bin_dir
   linux_puppet_bin_dir = '/opt/puppetlabs/puppet/bin'
-  windows_puppet_bin_dir = 'C:\\Program Files\\Puppet Labs\\Puppet\\bin'
+  windows_puppet_bin_dir = 'C:\\Program Files\\Puppet Labs\\Puppet\\bin\\'
 
   (HOST_PLATFORM.to_s.include? 'windows') ? windows_puppet_bin_dir : linux_puppet_bin_dir
 end
 
-def gem_command
-  return File.join(puppet_bin_dir, 'gem') unless HOST_PLATFORM.to_s.include? 'windows'
-  "C:\\Program Files\\Puppet Labs\\Puppet\\puppet\\bin\\gem"
-end
-
 def install_latest_facter_4(gem_command)
   message('BUILD FACTER 4 LATEST AGENT GEM')
-  run("#{gem_command} build agent/facter-ng.gemspec", ENV['FACTER_4_ROOT'])
+  run("#{gem_command} build agent\\facter-ng.gemspec", ENV['FACTER_4_ROOT'])
 
   message('UNINSTALL DEFAULT FACTER 4 AGENT GEM')
   run("#{gem_command} uninstall facter-ng")
@@ -113,10 +118,12 @@ def message(message)
   puts "\n\n#{result}\n\n"
 end
 
-def run(command, dir = './', env = {})
+def run(command, dir = './')
   puts command
   output = ''
   status = 0
+  env = {}
+  env = { 'PATH' => "C:\\Program Files\\Puppet Labs\\Puppet\\puppet\\bin;#{ENV['PATH']}" } if HOST_PLATFORM.to_s.include? 'windows'
   Open3.popen2e(env, command, chdir: dir) do |stdin, stdout_and_err, wait_thr|
     stdout_and_err.each do |line|
       puts line
@@ -137,8 +144,8 @@ Dir.chdir(FACTER_3_ACCEPTANCE_PATH) { install_facter_3_dependencies }
 install_custom_beaker
 
 Dir.chdir(FACTER_3_ACCEPTANCE_PATH) do
-  initialize_beaker
-  install_puppet_agent
+ initialize_beaker
+ install_puppet_agent
 end
 
 replace_facter_3_with_facter_4
