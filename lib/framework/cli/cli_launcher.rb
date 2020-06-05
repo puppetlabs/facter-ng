ee14069fb9f09e9bead323bf58b33696b5fec06b
BogdanIrimie <bogdan.irimie@puppet.com>
2020-06-05 11:47:30 +0300

    (FACT-2651) Reorder arguments based on priority.

--- lib/framework/cli/cli_launcher.rb
+++ lib/framework/cli/cli_launcher.rb
@@ -7,10 +7,38 @@ Facter::Log.output(STDERR)
 require "#{ROOT_DIR}/lib/facter"
 require "#{ROOT_DIR}/lib/framework/cli/cli"
 
+def check(known_arguments, program_arguments)
+  program_arguments.each do |argument|
+    return true if known_arguments.key?(argument)
+  end
+
+  return false
+end
+
+def reorder!(program_arguments)
+  priority_arguments = Facter::Cli.instance_variable_get(:@map)
+
+  priority_args = []
+  normal_args = []
+
+  program_arguments.each do |argument|
+    if priority_arguments.include?(argument)
+      priority_args << (argument)
+    else
+      normal_args << argument
+    end
+  end
+
+  priority_args.concat(normal_args)
+end
+
+
 Facter::OptionsValidator.validate(ARGV)
 ARGV.unshift(Facter::Cli.default_task) unless
   Facter::Cli.all_tasks.key?(ARGV[0]) ||
-  Facter::Cli.instance_variable_get(:@map).key?(ARGV[0])
+  check(Facter::Cli.instance_variable_get(:@map), ARGV)
+
+ARGV = reorder!(ARGV)
 
 begin
   Facter::Cli.start(ARGV, debug: true)
