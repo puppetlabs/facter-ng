0f7c1473a2b715d0228ebbc33eab18c2a83f7f6e
BogdanIrimie <bogdan.irimie@puppet.com>
2020-06-08 15:39:04 +0300

    (FACT-2651) Reorganise cli launcher in OO manner.

--- lib/framework/cli/cli_launcher.rb
+++ lib/framework/cli/cli_launcher.rb
@@ -7,40 +7,59 @@ Facter::Log.output(STDERR)
 require "#{ROOT_DIR}/lib/facter"
 require "#{ROOT_DIR}/lib/framework/cli/cli"
 
-def check(known_arguments, program_arguments)
-  program_arguments.each do |argument|
-    return true if known_arguments.key?(argument)
+class CliLauncher
+  def initialize(args)
+    @args = args
   end
 
-  false
-end
+  def validate_options
+    Facter::OptionsValidator.validate(@args)
+  end
 
-def reorder!(program_arguments)
-  priority_arguments = Facter::Cli.instance_variable_get(:@map)
+  def prepare_arguments
+    @args.unshift(Facter::Cli.default_task) unless
+      Facter::Cli.all_tasks.key?(@args[0]) ||
+      check_if_arguments_is_known(Facter::Cli.instance_variable_get(:@map), @args)
 
-  priority_args = []
-  normal_args = []
+    @args = reorder_program_arguments(@args)
+  end
 
-  program_arguments.each do |argument|
-    if priority_arguments.include?(argument)
-      priority_args << argument
-    else
-      normal_args << argument
+  def start
+    Facter::Cli.start(@args, debug: true)
+  rescue Thor::UnknownArgumentError => e
+    Facter::OptionsValidator.write_error_and_exit("unrecognised option '#{e.unknown.first}'")
+  end
+
+  private
+
+  def check_if_arguments_is_known(known_arguments, program_arguments)
+    program_arguments.each do |argument|
+      return true if known_arguments.key?(argument)
     end
+
+    false
   end
 
-  priority_args.concat(normal_args)
+  def reorder_program_arguments(program_arguments)
+    priority_arguments = Facter::Cli.instance_variable_get(:@map)
+
+    priority_args = []
+    normal_args = []
+
+    program_arguments.each do |argument|
+      if priority_arguments.include?(argument)
+        priority_args << argument
+      else
+        normal_args << argument
+      end
+    end
+
+    priority_args.concat(normal_args)
+  end
 end
 
-Facter::OptionsValidator.validate(ARGV)
-ARGV.unshift(Facter::Cli.default_task) unless
-  Facter::Cli.all_tasks.key?(ARGV[0]) ||
-  check(Facter::Cli.instance_variable_get(:@map), ARGV)
+cli_launcher = CliLauncher.new(ARGV)
 
-ARGV = reorder!(ARGV)
-
-begin
-  Facter::Cli.start(ARGV, debug: true)
-rescue Thor::UnknownArgumentError => e
-  Facter::OptionsValidator.write_error_and_exit("unrecognised option '#{e.unknown.first}'")
-end
+cli_launcher.validate_options
+cli_launcher.prepare_arguments
+cli_launcher.start
