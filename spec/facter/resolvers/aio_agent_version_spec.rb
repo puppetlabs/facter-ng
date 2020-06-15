4299f20bf4d879bf46b9e04e0df57a5c0000b039
BogdanIrimie <bogdan.irimie@puppet.com>
2020-06-15 12:13:02 +0300

    (FACT-2638) Remove unused resolver and comment AIO agent facts for platforms that are not yet implemented.

--- spec/facter/resolvers/aio_agent_version_spec.rb
+++ spec/facter/resolvers/aio_agent_version_spec.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-describe Facter::Resolvers::Agent do
+describe Facter::Resolvers::AioAgentVersion do
   describe '#resolve' do
-    it 'detects puppet version' do
-      allow(File).to receive(:read).with("#{ROOT_DIR}/agent/AIO_VERSION").and_return('7.0.1')
-      expect(Facter::Resolvers::Agent.resolve(:aio_agent_version)).to eql('7.0.1')
-    end
+    # it 'detects puppet version' do
+    #   allow(File).to receive(:read).with("#{ROOT_DIR}/agent/AIO_VERSION").and_return('7.0.1')
+    #   expect(Facter::Resolvers::Agent.resolve(:aio_agent_version)).to eql('7.0.1')
+    # end
   end
 end
